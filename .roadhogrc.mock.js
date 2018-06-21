3ac056f9c8401f94ea7f8302cd8326e1e8011298
wangyun <wangyunsxz@qq.com>
2018-06-21 11:25:45 +0800

    修改api

--- .roadhogrc.mock.js
+++ .roadhogrc.mock.js
@@ -20,67 +20,10 @@ const noProxy = process.env.NO_PROXY === 'true';
 const proxy = {
   // 支持值为 Object 和 Array
 
-<<<<<<< HEAD
   'GET /acloud_new/v2/(.*)': 'http://118.126.110.115:3001/mock/28/acloud_new/v2/',
   'POST /acloud_new/v2/(.*)': 'http://118.126.110.115:3001/mock/28/acloud_new/v2/',
   'PUT /acloud_new/v2/(.*)': 'http://118.126.110.115:3001/mock/28/acloud_new/v2/',
   'DELETE /acloud_new/v2/(.*)': 'http://118.126.110.115:3001/mock/28/acloud_new/v2/',
-
-  'GET /api/currentUser': {
-<<<<<<< HEAD
-    name: 'Serati Ma',
-    avatar: 'https://gw.alipayobjects.com/zos/rmsportal/BiazfanxmamNRoxxVxka.png',
-    userid: '00000001',
-    email: 'antdesign@alipay.com',
-    signature: '海纳百川，有容乃大',
-    title: '交互专家',
-    group: '蚂蚁金服－某某某事业群－某某平台部－某某技术部－UED',
-    tags: [
-      {
-        key: '0',
-        label: '很有想法的',
-      },
-      {
-        key: '1',
-        label: '专注设计',
-      },
-      {
-        key: '2',
-        label: '辣~',
-      },
-      {
-        key: '3',
-        label: '大长腿',
-      },
-      {
-        key: '4',
-        label: '川妹子',
-      },
-      {
-        key: '5',
-        label: '海纳百川',
-      },
-    ],
-    notifyCount: 12,
-    country: 'China',
-    geographic: {
-      province: {
-        label: '浙江省',
-        key: '330000',
-      },
-      city: {
-        label: '杭州市',
-        key: '330100',
-      },
-    },
-    address: '西湖区工专路 77 号',
-    phone: '0752-268888888',
-=======
-=======
-  'GET /acloud_new/api/(.*)': 'http://118.126.110.115:3001/mock/28/acloud_new/v2/',
-  'POST /acloud_new/api/(.*)': 'http://118.126.110.115:3001/mock/28/acloud_new/v2/',
-  'PUT /acloud_new/api/(.*)': 'http://118.126.110.115:3001/mock/28/acloud_new/v2/',
-  'DELETE /acloud_new/api/(.*)': 'http://118.126.110.115:3001/mock/28/acloud_new/v2/',
 
   'GET /v2/currentUser': {
 >>>>>>> lazyload
