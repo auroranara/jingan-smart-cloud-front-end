96cff3f8a1c4ff256eaee8e53888554b11bf91e1
Unknown <18921261761@163.com>
2018-07-06 13:20:56 +0800

    详情、新建、编辑页面

--- src/models/contract.js
+++ src/models/contract.js
@@ -1,4 +1,4 @@
-// import { queryContractList, queryStatusList } from '../services/contract/contract.js';
+// import { queryContractList, queryStatusList, queryContract, addContract, queryMaintanceList, queryServiceList } from '../services/contract/contract.js';
 
 export default {
   namespace: 'contract',
@@ -9,11 +9,17 @@ export default {
       pagination: {
         total: 0,
         pageSize: 1,
-        pageNum: 24,
+        pageNum: 1,
       },
     },
     isLast: false,
     statusList: [],
+    detail: {},
+    maintanceList: [{
+      id: 1,
+      name: '陆华',
+    }],
+    serviceList: [],
   },
 
   effects: {
@@ -26,9 +32,12 @@ export default {
           list: [{
             id: 1,
             name: '陆华',
+            number: 'x00001',
+            safetyName: '陆华',
+            safetyPhone: '15911111111',
           }],
           pagination: {
-            total: 0,
+            total: 2,
             pageSize: payload.pageSize,
             pageNum: payload.pageNum,
           },
@@ -37,6 +46,36 @@ export default {
       if (response.code === 200) {
         yield put({
           type: 'queryList',
+          payload: response.data,
+        });
+        if (success) {
+          success();
+        }
+      } else if (error) {
+        error(response.msg);
+      }
+    },
+    /* 追加维保合同列表 */
+    *appendList({ payload, success, error }, { put }) {
+      // const response = yield call(queryContractList, payload);
+      const response = {
+        code: 200,
+        data: {
+          list: [{
+            id: 2,
+            name: '虫二',
+            number: 'x00001',
+          }],
+          pagination: {
+            total: 2,
+            pageSize: payload.pageSize,
+            pageNum: payload.pageNum,
+          },
+        },
+      };
+      if (response.code === 200) {
+        yield put({
+          type: 'pushList',
           payload: response.data,
         });
         if (success) {
@@ -61,6 +100,9 @@ export default {
           }, {
             id: 3,
             label: '即将过期',
+          }, {
+            id: 4,
+            label: '未开始',
           }],
           pagination: {
             total: 0,
@@ -72,6 +114,125 @@ export default {
       if (response.code === 200) {
         yield put({
           type: 'queryStatusList',
+          payload: response.data.list,
+        });
+        if (success) {
+          success();
+        }
+      } else if (error) {
+        error(response.msg);
+      }
+    },
+    /* 获取合同详情 */
+    *fetchContract({ payload, success, error }, { put }) {
+      // const response = yield call(queryContract, payload);
+      const response = {
+        code: 200,
+        data: {
+          id: payload.id,
+          name: '陆华',
+          number: 'x00001',
+        },
+      };
+      if (response.code === 200) {
+        yield put({
+          type: 'queryContract',
+          payload: response.data,
+        });
+        if (success) {
+          success(response.data);
+        }
+      } else if (error) {
+        error(response.msg);
+      }
+    },
+    /* 新增合同 */
+    *insertContract({ payload, success, error }, { put }) {
+      // const response = yield call(addContract, payload);
+      const response = {
+        code: 200,
+        data: {
+          id: payload.id,
+          name: '陆华',
+          number: 'x00001',
+        },
+      };
+      if (response.code === 200) {
+        yield put({
+          type: 'addContract',
+          payload: response.data,
+        });
+        if (success) {
+          success();
+        }
+      } else if (error) {
+        error(response.msg);
+      }
+    },
+    /* 修改合同 */
+    *updateContract({ payload, success, error }, { put }) {
+      // const response = yield call(addContract, payload);
+      const response = {
+        code: 200,
+        data: {
+          id: payload.id,
+          name: '陆华',
+          number: 'x00001',
+        },
+      };
+      if (response.code === 200) {
+        yield put({
+          type: 'editContract',
+          payload: response.data,
+        });
+        if (success) {
+          success();
+        }
+      } else if (error) {
+        error(response.msg);
+      }
+    },
+    *fetchMaintanceList({ payload, success, error }, { put }) {
+      // const response = yield call(queryMaintanceList, payload);
+      const response = {
+        code: 200,
+        data: {
+          list: [],
+          pagination: {
+            total: 0,
+            pageSize: payload.pageSize,
+            pageNum: payload.pageNum,
+          },
+        },
+      };
+      if (response.code === 200) {
+        yield put({
+          type: 'queryMaintanceList',
+          payload: response.data.list,
+        });
+        if (success) {
+          success();
+        }
+      } else if (error) {
+        error(response.msg);
+      }
+    },
+    *fetchServiceList({ payload, success, error }, { put }) {
+      // const response = yield call(queryServiceList, payload);
+      const response = {
+        code: 200,
+        data: {
+          list: [],
+          pagination: {
+            total: 0,
+            pageSize: payload.pageSize,
+            pageNum: payload.pageNum,
+          },
+        },
+      };
+      if (response.code === 200) {
+        yield put({
+          type: 'queryServiceList',
           payload: response.data.list,
         });
         if (success) {
@@ -93,11 +254,65 @@ export default {
         isLast: pageNum * pageSize >= total,
       };
     },
+    /* 追加合同列表 */
+    pushList(state, { payload: { list, pagination } }) {
+      const { pageNum, pageSize, total } = pagination;
+      return {
+        ...state,
+        data: {
+          list: state.data.list.concat(list),
+          pagination,
+        },
+        isLast: pageNum * pageSize >= total,
+      };
+    },
     /* 获取单位状态列表 */
     queryStatusList(state, { payload }) {
       return {
         ...state,
         statusList: payload,
+      };
+    },
+    /* 获取合同详情 */
+    queryContract(state, { payload }) {
+      return {
+        ...state,
+        detail: payload,
+      };
+    },
+    /* 新增合同 */
+    addContract(state, { payload }) {
+      return {
+        ...state,
+        detail: payload,
+      };
+    },
+    /* 编辑合同 */
+    editContract(state, { payload }) {
+      return {
+        ...state,
+        detail: payload,
+      };
+    },
+    /* 清空合同 */
+    clearDetail(state) {
+      return {
+        ...state,
+        detail: {},
+      };
+    },
+    /* 获取维保单位列表 */
+    queryMaintanceList(state, { payload }) {
+      return {
+        ...state,
+        maintanceList: payload,
+      };
+    },
+    /* 获取维保单位列表 */
+    queryServiceList(state, { payload }) {
+      return {
+        ...state,
+        serviceList: payload,
       };
     },
   },
