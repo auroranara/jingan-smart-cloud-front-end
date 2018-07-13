f2c37077afb3ee37b4f94a5d4bc80746708a1b46
wangyun <wangyunsxz@qq.com>
2018-07-13 21:05:21 +0800

    build: finish login and register page

--- src/pages/layouts/UserLayout.js
+++ src/pages/layouts/UserLayout.js
@@ -1,16 +1,16 @@
 import React, { Fragment } from 'react';
 import { Link } from 'dva/router';
-import DocumentTitle from 'react-document-title';
+// import DocumentTitle from 'react-document-title';
 import { Icon } from 'antd';
-import GlobalFooter from '../components/GlobalFooter';
+import GlobalFooter from '../../components/GlobalFooter';
 import styles from './UserLayout.less';
-import logo from '../assets/logo.svg';
+import logo from '../../assets/logo.svg';
 
 // TODO:remove
 // import { getRoutes, getPageQuery, getQueryPath } from '../utils/utils';
 
 const links = [
-  /* {
+  {
     key: 'help',
     title: '帮助',
     href: '',
@@ -24,13 +24,12 @@ const links = [
     key: 'terms',
     title: '条款',
     href: '',
-  }, */
+  },
 ];
 
 const copyright = (
   <Fragment>
-    <p>Copyright <Icon type="copyright" /> 2018 &nbsp;技术支持：晶安智慧科技有限公司</p>
-    <p style={{ marginTop: '5px' }}>服务电话：400-928-5656</p>
+    Copyright <Icon type="copyright" /> 2018 晶安科技有限公司
   </Fragment>
 );
 // TODO:remove
@@ -43,32 +42,34 @@ const copyright = (
 // }
 
 class UserLayout extends React.PureComponent {
-  getPageTitle() {
-    const { routerData, location } = this.props;
-    const { pathname } = location;
-    let title = '晶安智慧云';
-    if (routerData[pathname] && routerData[pathname].name) {
-      title = `${routerData[pathname].name} - 晶安智慧云`;
-    }
-    return title;
-  }
+  // TODO title
+  // getPageTitle() {
+  //   const { routerData, location } = this.props;
+  //   const { pathname } = location;
+  //   let title = 'Ant Design Pro';
+  //   if (routerData[pathname] && routerData[pathname].name) {
+  //     title = `${routerData[pathname].name} - Ant Design Pro`;
+  //   }
+  //   return title;
+  // }
 
   render() {
     const { children } = this.props;
     return (
-      <DocumentTitle title={this.getPageTitle()}>
-        <div className={styles.container}>
-          <div className={styles.content}>
-            <div className={styles.top}>
-              <div className={styles.header}>
-                <Link to="/">
-                  <img alt="logo" className={styles.logo} src={logo} />
-                  <span className={styles.title}>晶安智慧云平台</span>
-                </Link>
-              </div>
+      // TODO <DocumentTitle title={this.getPageTitle()}>
+      <div className={styles.container}>
+        <div className={styles.content}>
+          <div className={styles.top}>
+            <div className={styles.header}>
+              <Link to="/">
+                <img alt="logo" className={styles.logo} src={logo} />
+                <span className={styles.title}>Ant Design</span>
+              </Link>
             </div>
-            {children}
-            {/* <Switch>
+            <div className={styles.desc} />
+          </div>
+          {children}
+          {/* <Switch>
               {getRoutes(match.path, routerData).map(item => (
                 <Route
                   key={item.key}
@@ -79,10 +80,10 @@ class UserLayout extends React.PureComponent {
               ))}
               <Redirect from="/user" to={getLoginPathWithRedirectPath()} />
             </Switch> */}
-          </div>
-          <GlobalFooter links={links} copyright={copyright} />
         </div>
-      </DocumentTitle>
+        <GlobalFooter links={links} copyright={copyright} />
+      </div>
+      // </DocumentTitle>
     );
   }
 }
