2e13fb89149adcd22ac2547962bc634408b44c0d
zangyunchong <13815207752@163.com>
2018-07-30 16:00:16 +0800

    feat: add part

--- src/pages/BaseInfo/Company/DepartmentList.js
+++ src/pages/BaseInfo/Company/DepartmentList.js
@@ -1,5 +1,5 @@
-import React, { PureComponent } from 'react';
-import { Form, Card, Input, Button, Col, Table } from 'antd'
+import React, { PureComponent, Fragment } from 'react';
+import { Form, Card, Input, Button, Table } from 'antd'
 import { connect } from 'dva';
 import PageHeaderLayout from '../../layouts/PageHeaderLayout.js';
 
@@ -69,8 +69,56 @@ export default class DepartmentList extends PureComponent {
 
   renderTable() {
 
+    const columns = [
+      {
+        title: '部门名称',
+        dataIndex: 'name',
+        key: 'name',
+      },
+      {
+        title: '账号数量',
+        dataIndex: 'number',
+        key: 'number',
+        width: '12%',
+      },
+      {
+        title: '操作',
+        key: '操作',
+        width: '30%',
+        render: (val, rows) => (
+          <Fragment>
+            <a >添加</a>
+            <a >编辑</a>
+            <a >删除</a>
+          </Fragment>
+        ),
+      },
+    ]
+
+    const data = [
+      {
+        id: '001',
+        name: 'test01',
+        number: 123,
+        children: [
+          {
+            id: 'oo2',
+            name: 'test02',
+            number: 45,
+          },
+        ],
+      },
+      {
+        id: '003',
+        name: 'test03',
+        number: 445,
+      },
+    ]
+
     return (
-      <Card></Card>
+      <Card style={{ marginTop: '20px' }}>
+        <Table rowKey='id' columns={columns} dataSource={data}></Table>
+      </Card>
     )
   }
 
