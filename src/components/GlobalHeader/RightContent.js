4ece64622846a1b457d4ec8e8351962c01ade201
wangyun <wangyunsxz@qq.com>
2018-08-06 15:45:59 +0800

    fix: 删除个人中心

--- src/components/GlobalHeader/RightContent.js
+++ src/components/GlobalHeader/RightContent.js
@@ -69,10 +69,6 @@ export default class GlobalHeaderRight extends PureComponent {
     } = this.props;
     const menu = (
       <Menu className={styles.menu} selectedKeys={[]} onClick={onMenuClick}>
-        <Menu.Item key="userCenter">
-          <Icon type="user" />
-          <FormattedMessage id="menu.account.center" defaultMessage="account center" />
-        </Menu.Item>
         <Menu.Item key="changePassword">
           <Icon type="lock" />
           <FormattedMessage
