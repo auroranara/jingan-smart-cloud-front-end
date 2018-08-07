3c321dd97465d35bacd9a8827ff6887ab015174a
zangyunchong <13815207752@163.com>
2018-08-07 15:27:27 +0800

    fix: 修改消防和安全大屏默认参数

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,56 +1,50 @@
 import React, { PureComponent } from 'react';
-import { connect } from 'dva';
-import Carousel3d from './Carousel3d';
-import styles from './Dashboard.less';
-import { getToken } from 'utils/authority';
-import fire from '../../assets/fire-big-screen.png';
-import safe from '../../assets/safe-bing-screen.png';
+import { connect } from 'dva'
+import Carousel3d from './Carousel3d'
+import styles from './Dashboard.less'
+import { getToken } from 'utils/authority'
+import fire from '../../assets/fire-big-screen.png'
+import safe from '../../assets/safe-bing-screen.png'
 
-const maxLength = 6; // 最多容纳个数
+const maxLength = 6 // 最多容纳个数
 
 @connect(({ user }) => ({
   user,
 }))
+
 export default class Dashboard extends PureComponent {
   state = {
     current: 0,
-  };
+  }
 
   handleChange = data => {
     if (data.eventType === 'end') {
-      this.setState({ current: data.current });
+      this.setState({ current: data.current })
     }
-  };
+  }
 
   render() {
-    const { current } = this.state;
+    const { current } = this.state
     const {
       user: {
-        currentUser: { companyBasicInfo },
+        currentUser: { companyBasicInfo: { safetyProduction, fireService } = {} },
       },
     } = this.props;
 
-    let safetyProduction, fireService;
-    if (companyBasicInfo) {
-      safetyProduction = companyBasicInfo.safetyProduction;
-      fireService = companyBasicInfo.fireService;
-    }
-    const safeItem = { src: safe, url: 'http://www.baidu.com' };
-    const fireItem = { src: fire, url: `/acloud_new/v2/hdf/fireIndex.htm?token=${getToken()}` };
+    const safeItem = { src: safe, url: 'http://www.baidu.com' }
+    const fireItem = { src: fire, url: `/acloud_new/v2/hdf/fireIndex.htm?token=${getToken()}` }
 
-    // safetyProduction,fireService 0开启/1关闭
-    const imgWrapper =
-      (safetyProduction === 0 &&
-        fireService === 0 && [safeItem, fireItem, safeItem, fireItem, safeItem, fireItem]) ||
-      (safetyProduction === 0 && fireService === 1 && [safeItem, safeItem, safeItem, safeItem]) ||
-      (safetyProduction === 1 && fireService === 0 && [fireItem, fireItem, fireItem, fireItem]) ||
-      [];
+    // safetyProduction,fireService 1开启/0关闭
+    const imgWrapper = (safetyProduction === 1 && fireService === 1 && [safeItem, fireItem, safeItem, fireItem, safeItem, fireItem])
+      || (safetyProduction === 1 && fireService === 0 && [safeItem, safeItem, safeItem, safeItem])
+      || (safetyProduction === 0 && fireService === 1 && [fireItem, fireItem, fireItem, fireItem])
+      || []
 
     const goToBigScreen = () => {
-      const url = imgWrapper[current].url;
+      const url = imgWrapper[current].url
       const win = window.open(url, '_blank');
       win.focus();
-    };
+    }
 
     const children = imgWrapper.map((item, i) => (
       <div
@@ -64,14 +58,10 @@ export default class Dashboard extends PureComponent {
     ));
     return (
       <div className={styles.carouselDemoWrapper}>
-        <Carousel3d
-          className={styles.carouselDemo}
-          onChange={this.handleChange}
-          childMaxLength={maxLength}
-        >
+        <Carousel3d className={styles.carouselDemo} onChange={this.handleChange} childMaxLength={maxLength}>
           {children}
         </Carousel3d>
       </div>
-    );
+    )
   }
 }
