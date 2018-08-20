5f3d6fc24e964a9d5718a4582356d73ad825d483
wangyun <wangyunsxz@qq.com>
2018-08-20 11:20:13 +0800

    feat: map

--- src/pages/BigPlatform/FireControl/Government.js
+++ src/pages/BigPlatform/FireControl/Government.js
@@ -10,6 +10,7 @@ import AlarmSection from './AlarmSection';
 import SystemSection from './SystemSection';
 import TrendSection from './TrendSection';
 import GridDangerSection from './GridDangerSection';
+import FireControlMap from './FireControlMap';
 import bg from './bg.png';
 
 const HEIGHT_PERCNET = { height: '100%' };
@@ -28,16 +29,27 @@ export default class FireControlBigPlatform extends PureComponent {
   }
 
   render() {
-    const { bigFireControl: { overview, alarm, sys, trend }, dispatch } = this.props;
+    const {
+      bigFireControl: { overview, alarm, sys, trend },
+      dispatch,
+    } = this.props;
 
     return (
-      <Row style={{ height: '100%', marginLeft: 0, marginRight: 0, background: `url(${bg}) center center` }} gutter={{ xs: 4, sm: 8, md: 12, lg: 16 }}>
+      <Row
+        style={{
+          height: '100%',
+          marginLeft: 0,
+          marginRight: 0,
+          background: `url(${bg}) center center`,
+        }}
+        gutter={{ xs: 4, sm: 8, md: 12, lg: 16 }}
+      >
         <Col span={6} style={HEIGHT_PERCNET}>
           <FcModule className={styles.overview}>
             <OverviewSection ovData={overview} />
             <FcSection title="辖区概况反面" isBack />
           </FcModule>
-          <div className={styles.gutter1}></div>
+          <div className={styles.gutter1} />
           <FcModule className={styles.alarmInfo}>
             <AlarmSection alarmData={alarm} dispatch={dispatch} />
             <FcSection title="警情信息反面" isBack />
@@ -45,10 +57,9 @@ export default class FireControlBigPlatform extends PureComponent {
         </Col>
         <Col span={12} style={HEIGHT_PERCNET}>
           <FcModule className={styles.map}>
-            <FcSection title="Map" />
-            <FcSection title="Map Reverse" isBack />
+            <FireControlMap />
           </FcModule>
-          <div className={styles.gutter2}></div>
+          <div className={styles.gutter2} />
           <Row className={styles.center}>
             <Col span={12} className={styles.centerLeft}>
               <FcModule style={{ height: '100%' }}>
@@ -69,7 +80,7 @@ export default class FireControlBigPlatform extends PureComponent {
             <FcSection title="单位查岗" />
             <FcSection title="单位查岗反面" isBack />
           </FcModule>
-          <div className={styles.gutter3}></div>
+          <div className={styles.gutter3} />
           <FcModule className={styles.system}>
             <SystemSection sysData={sys} />
             <FcSection title="系统接入情况反面" isBack />
