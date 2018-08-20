77b33ce2d790ca0a1b96620b50dc4e1016394d0b
yulinzkg <yulinzkg@126.com>
2018-08-20 11:56:33 +0800

    !26 政府安全大屏 Merge pull request !26 from wangyun/feat/safety-gov

--- config/config.js
+++ config/config.js
@@ -20,7 +20,7 @@ const hosts = {
 export default {
   proxy: {
     '/acloud_new': {
-      target: `http://192.168.10.6`,
+      target: `http://${hosts.dev}`,
       changeOrigin: true,
       pathRewrite: { '^/acloud_new': '/acloud_new' },
     },
@@ -30,7 +30,7 @@ export default {
       pathRewrite: { '^/mock': '/mock' },
     },
     '/gsafe': {
-      target: `http://${hosts.test}`,
+      target: `http://${hosts.dev}`,
       changeOrigin: true,
       pathRewrite: { '^/gsafe': '/gsafe' },
     },
