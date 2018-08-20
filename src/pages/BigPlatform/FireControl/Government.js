d557098d090eeae80c32e0fa86e34c0a9946999a
neo <neo.lu@foxmail.com>
2018-08-20 14:30:47 +0800

    feat(big-fire): store temp

--- src/pages/BigPlatform/FireControl/Government.js
+++ src/pages/BigPlatform/FireControl/Government.js
@@ -3,6 +3,7 @@ import { connect } from 'dva';
 import { Col, Row } from 'antd';
 
 import styles from './Government.less';
+import Head from './Head';
 import FcModule from './FcModule';
 import FcSection from './FcSection';
 import OverviewSection from './OverviewSection';
@@ -32,51 +33,55 @@ export default class FireControlBigPlatform extends PureComponent {
     const { bigFireControl: { overview, alarm, sys, trend, danger }, dispatch } = this.props;
 
     return (
-      <Row style={{ height: '100%', marginLeft: 0, marginRight: 0, background: `url(${bg}) center center` }} gutter={{ xs: 4, sm: 8, md: 12, lg: 16 }}>
-        <Col span={6} style={HEIGHT_PERCNET}>
-          <FcModule className={styles.overview}>
-            <OverviewSection ovData={overview} />
-            <FcSection title="辖区概况反面" isBack />
-          </FcModule>
-          <div className={styles.gutter1}></div>
-          <FcModule className={styles.alarmInfo}>
-            <AlarmSection alarmData={alarm} dispatch={dispatch} />
-            <FcSection title="警情信息反面" isBack />
-          </FcModule>
-        </Col>
-        <Col span={12} style={HEIGHT_PERCNET}>
-          <FcModule className={styles.map}>
-            <FcSection title="Map" />
-            <FcSection title="Map Reverse" isBack />
-          </FcModule>
-          <div className={styles.gutter2}></div>
-          <Row className={styles.center}>
-            <Col span={12} className={styles.centerLeft}>
-              <FcModule style={{ height: '100%' }}>
-                <TrendSection trendData={trend} />
-                <FcSection title="火警趋势反面" isBack />
-              </FcModule>
-            </Col>
-            <Col span={12} className={styles.centerRight}>
-              <FcModule style={{ height: '100%' }}>
-                <GridDangerSection dangerData={danger} />
-                <FcSection title="网格隐患巡查反面" isBack />
-              </FcModule>
-            </Col>
-          </Row>
-        </Col>
-        <Col span={6} style={HEIGHT_PERCNET}>
-          <FcModule className={styles.inspect}>
-            <FcSection title="单位查岗" />
-            <FcSection title="单位查岗反面" isBack />
-          </FcModule>
-          <div className={styles.gutter3}></div>
-          <FcModule className={styles.system}>
-            <SystemSection sysData={sys} />
-            <FcSection title="系统接入情况反面" isBack />
-          </FcModule>
-        </Col>
-      </Row>
+      <div className={styles.root} style={{ background: `url(${bg}) center center` }}>
+        <Head title="晶安智慧消防云平台" />
+        <div className={styles.empty} />
+        <Row style={{ height: '88%', marginLeft: 0, marginRight: 0 }} gutter={{ xs: 4, sm: 8, md: 12, lg: 16 }}>
+          <Col span={6} style={HEIGHT_PERCNET}>
+            <FcModule className={styles.overview}>
+              <OverviewSection ovData={overview} />
+              <FcSection title="辖区概况反面" isBack />
+            </FcModule>
+            <div className={styles.gutter1}></div>
+            <FcModule className={styles.alarmInfo}>
+              <AlarmSection alarmData={alarm} dispatch={dispatch} />
+              <FcSection title="警情信息反面" isBack />
+            </FcModule>
+          </Col>
+          <Col span={12} style={HEIGHT_PERCNET}>
+            <FcModule className={styles.map}>
+              <FcSection title="Map" />
+              <FcSection title="Map Reverse" isBack />
+            </FcModule>
+            <div className={styles.gutter2}></div>
+            <Row className={styles.center}>
+              <Col span={12} className={styles.centerLeft}>
+                <FcModule style={{ height: '100%' }}>
+                  <TrendSection trendData={trend} />
+                  <FcSection title="火警趋势反面" isBack />
+                </FcModule>
+              </Col>
+              <Col span={12} className={styles.centerRight}>
+                <FcModule style={{ height: '100%' }}>
+                  <GridDangerSection dangerData={danger} />
+                  <FcSection title="网格隐患巡查反面" isBack />
+                </FcModule>
+              </Col>
+            </Row>
+          </Col>
+          <Col span={6} style={HEIGHT_PERCNET}>
+            <FcModule className={styles.inspect}>
+              <FcSection title="单位查岗" />
+              <FcSection title="单位查岗反面" isBack />
+            </FcModule>
+            <div className={styles.gutter3}></div>
+            <FcModule className={styles.system}>
+              <SystemSection sysData={sys} />
+              <FcSection title="系统接入情况反面" isBack />
+            </FcModule>
+          </Col>
+        </Row>
+      </div>
     );
   }
 }
