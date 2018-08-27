1e98ba5c518877f3b9b1aa2e130d4457030234ed
sk <18921261761@163.com>
2018-08-27 16:51:26 +0800

    feat(unit-fire-control): 临时上传

--- src/pages/BigPlatform/UnitFireControl/UnitFireControl.js
+++ src/pages/BigPlatform/UnitFireControl/UnitFireControl.js
@@ -5,6 +5,29 @@ import Section from './components/Section/Section';
 
 import styles from './UnitFireControl.less';
 
+const prefix = 'http://data.jingan-china.cn/v2/big-platform/fire-control/com/';
+const fireIcon = `${prefix}fire_hj.png`;
+const faultIcon = `${prefix}fire_gz.png`;
+const positionBlueIcon = `${prefix}fire_position_blue.png`;
+const positionRedIcon = `${prefix}fire_position_red.png`;
+const triangleIcon = `${prefix}triangle.png`;
+const dfcIcon = `${prefix}fire_dfc.png`;
+const wcqIcon = `${prefix}fire_wcq.png`;
+const ycqIcon = `${prefix}fire_ycq.png`;
+/* 待处理信息项 */
+const pendingInfoItem = ({ id }) => {
+  return (
+    <div key={id} className={styles.pendingInfoItem}>
+      <div>
+        <div></div>
+      </div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
+
 /**
  * 单位消防大屏
  */
@@ -40,7 +63,13 @@ export default class App extends PureComponent {
         <div className={styles.mainBody}>
           <Row gutter={16} style={{ marginBottom: 16, height: 'calc(48.92% - 16px)' }}>
             <Col span={6} style={{ height: '100%' }}>
-              <Section style={{  }} />
+              <Section style={{  }} isScroll isCarousel>
+                {[...Array(50).keys()].map((item) => (
+                  <div key={item}>
+                    {item}
+                  </div>
+                ))}
+              </Section>
             </Col>
             <Col span={12} style={{ height: '100%' }}>
               <Section style={{  }} />
--- src/pages/BigPlatform/UnitFireControl/components/Section/Section.js
+++ src/pages/BigPlatform/UnitFireControl/components/Section/Section.js
@@ -5,13 +5,132 @@ import styles from './Section.less';
  * 大屏块
  */
 export default class App extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      // 是否显示滚动条
+      isScrollShow: false,
+      // 是否显示占位内容
+      isPlaceHolderShow: false,
+    };
+    // 轮播定时器
+    this.carouselTimer = null;
+  }
+
+  /**
+   * 组件挂载
+   */
+  componentDidMount() {
+    // 添加轮播
+    this.addCarousel();
+  }
+
+  /**
+   * 组件销毁
+   */
+  componentWillUnmount() {
+    // 清除轮播
+    this.clearCarousel();
+  }
+
+  /**
+   * 添加轮播定时器
+   */
+  addCarousel = () => {
+    const { isCarousel } = this.props;
+    if (isCarousel) {
+      this.carouselTimer = setInterval(() => {
+        const scrollTop = this.list.scrollTop;
+        const scrollHeight = this.list.scrollHeight;
+        const offsetHeight = this.list.offsetHeight;
+        if (offsetHeight < scrollHeight) {
+          if (scrollTop < scrollHeight / 2) {
+            this.list.scrollTop = scrollTop + 1;
+          }
+          else {
+            this.list.scrollTop = scrollTop - scrollHeight / 2;
+          }
+        }
+      }, 25);
+      if (this.list.offsetHeight < this.list.scrollHeight) {
+        this.showPlaceHolder();
+      }
+    }
+  }
+
+  /**
+   * 清除轮播定时器
+   */
+  clearCarousel = () => {
+    clearInterval(this.carouselTimer);
+  }
+
+  /**
+   * 显示滚动条
+   */
+  showScroll = () => {
+    this.setState({
+      isScrollShow: true,
+    });
+  }
+
+  /**
+   * 隐藏滚动条
+   */
+  hideScroll = () => {
+    this.setState({
+      isScrollShow: false,
+    });
+  }
+
+  /**
+   * 显示占位内容
+   */
+  showPlaceHolder = () => {
+    this.setState({
+      isPlaceHolderShow: true,
+    });
+  }
+
+  /**
+   * 隐藏占位内容
+   */
+  hidePlaceHolder = () => {
+    this.setState({
+      isPlaceHolderShow: false,
+    });
+  }
+
+  /**
+   * 鼠标移入事件
+   */
+  handleMouseEnter = () => {
+    // 清除轮播
+    this.clearCarousel();
+    // 显示滚动条
+    this.showScroll();
+    // 隐藏占位内容
+    // this.hidePlaceHolder();
+  }
+
+  /**
+   * 鼠标移出事件
+   */
+  handleMouseLeave = () => {
+    // 添加轮播
+    this.addCarousel();
+    // 隐藏滚动条
+    this.hideScroll();
+    // 显示占位内容
+    // this.showPlaceHolder();
+  }
 
   /**
    * 渲染函数
    */
   render() {
-    const { isScroll, title, fixedContent, children, className, style } = this.props;
-    const scrollClassName = isScroll ? `${styles.wrapper} ${styles.scroll}` : styles.wrapper;
+    const { isScroll, isCarousel, title, fixedContent, children, className, style } = this.props;
+    const { isScrollShow, isPlaceHolderShow } = this.state;
     const outerClassName = className ? `${styles.outer} ${className}` : styles.outer;
 
     return (
@@ -28,8 +147,17 @@ export default class App extends PureComponent {
               {fixedContent}
             </div>
           )}
-          <div className={scrollClassName}>
+          <div
+            className={styles.scroll}
+            style={{
+              overflowY: isScroll ? (isCarousel ? (isScrollShow ? 'auto' : undefined) : 'auto') : undefined,
+            }}
+            onMouseEnter={isCarousel ? this.handleMouseEnter : undefined}
+            onMouseLeave={isCarousel ? this.handleMouseLeave : undefined}
+            ref={list=>this.list=list}
+          >
             {children}
+            {isScroll && isCarousel && isPlaceHolderShow && children}
           </div>
         </div>
       </section>
