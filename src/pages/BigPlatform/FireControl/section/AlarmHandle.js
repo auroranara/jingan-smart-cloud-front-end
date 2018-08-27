6ca0ba1dec59a399d1d595c17b52af50f0c41364
pcy <cypan@jingan-china.net>
2018-08-27 17:41:51 +0800

    feat(fire): add alarmHandle

--- src/pages/BigPlatform/FireControl/section/AlarmHandle.js
+++ src/pages/BigPlatform/FireControl/section/AlarmHandle.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Col, Row, Timeline, Button } from 'antd';
+import { Row, Timeline, Carousel } from 'antd';
 import FcSection from './FcSection';
 import styles from './AlarmHandle.less';
 
@@ -17,16 +17,57 @@ export default class AlarmHandle extends Component {
         <section>
           <Row>
             <div className={styles.top}>
-              <Timeline pending="" reverse={this.state.reverse}>
+              <Timeline
+                pending=""
+                reverse={this.state.reverse}
+                style={{ marginLeft: 60, marginTop: 10 }}
+              >
                 <Timeline.Item>
-                  <span />
-                  <span />
-                  <p />
+                  <span className={styles.time} style={{ color: '#fff' }}>
+                    10:00:00
+                  </span>
+                  <div>
+                    <span className={styles.status} style={{ color: '#fff' }}>
+                      报警
+                    </span>
+                  </div>
+                  <div>
+                    <p className={styles.content} style={{ color: '#fff' }}>
+                      上报该火警为真实火警
+                    </p>
+                    <p className={styles.contact} style={{ color: '#4f6793' }}>
+                      <span>安全员：张三</span>
+                      <span className={styles.phone}>17777777777</span>
+                    </p>
+                  </div>
+                </Timeline.Item>
+                <Timeline.Item>
+                  <span className={styles.time} style={{ color: '#fff' }}>
+                    10:00:00
+                  </span>
+                  <div>
+                    <span className={styles.status} style={{ color: '#fff' }}>
+                      报警
+                    </span>
+                  </div>
+                  <div>
+                    <p className={styles.content} style={{ color: '#fff' }}>
+                      上报该火警为真实火警
+                    </p>
+                    <p className={styles.contact} style={{ color: '#4f6793' }}>
+                      <span>安全员：张三</span>
+                      <span className={styles.phone}>17777777777</span>
+                    </p>
+                  </div>
                 </Timeline.Item>
               </Timeline>
             </div>
           </Row>
-          <div className={styles.bottom} />
+          <div className={styles.bottom}>
+            <div className={styles.arrowRight} />
+            <div className={styles.picMain} />
+            <div className={styles.arrowLeft} />
+          </div>
         </section>
       </FcSection>
     );
