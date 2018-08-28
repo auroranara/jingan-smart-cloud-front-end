0ac24c3e94017d66e12735b9d49f52ca24df39ae
pcy <cypan@jingan-china.net>
2018-08-28 17:32:20 +0800

    feat(fire): add alarmHandle

--- src/pages/BigPlatform/FireControl/section/AlarmHandle.js
+++ src/pages/BigPlatform/FireControl/section/AlarmHandle.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
-import { Row, Timeline, Carousel } from 'antd';
+import { Row, Timeline, Col } from 'antd';
 import FcSection from './FcSection';
 import styles from './AlarmHandle.less';
 
+import arrowLeft from '../img/arrowLeft.png';
+import arrowRight from '../img/arrowRight.png';
+import imgBg from '../img/imgBg.png';
 export default class AlarmHandle extends Component {
   state = {
     // 用于控制节点为正序排列
@@ -20,9 +23,9 @@ export default class AlarmHandle extends Component {
               <Timeline
                 pending=""
                 reverse={this.state.reverse}
-                style={{ marginLeft: 60, marginTop: 10 }}
+                style={{ marginLeft: 65, marginTop: 10 }}
               >
-                <Timeline.Item>
+                <Timeline.Item style={{ paddingBottom: 22 }}>
                   <span className={styles.time} style={{ color: '#fff' }}>
                     10:00:00
                   </span>
@@ -35,19 +38,15 @@ export default class AlarmHandle extends Component {
                     <p className={styles.content} style={{ color: '#fff' }}>
                       上报该火警为真实火警
                     </p>
-                    <p className={styles.contact} style={{ color: '#4f6793' }}>
-                      <span>安全员：张三</span>
-                      <span className={styles.phone}>17777777777</span>
-                    </p>
                   </div>
                 </Timeline.Item>
-                <Timeline.Item>
+                <Timeline.Item style={{ paddingBottom: 10 }}>
                   <span className={styles.time} style={{ color: '#fff' }}>
                     10:00:00
                   </span>
                   <div>
                     <span className={styles.status} style={{ color: '#fff' }}>
-                      报警
+                      上报
                     </span>
                   </div>
                   <div>
@@ -60,14 +59,68 @@ export default class AlarmHandle extends Component {
                     </p>
                   </div>
                 </Timeline.Item>
+                <Timeline.Item style={{ paddingBottom: 10 }}>
+                  <span className={styles.time} style={{ color: '#fff' }}>
+                    10:00:00
+                  </span>
+                  <div>
+                    <span className={styles.bestatus} style={{ color: '#fff' }}>
+                      处理
+                    </span>
+                  </div>
+                  <div>
+                    <p className={styles.content} style={{ color: '#4f6793' }}>
+                      暂未处理完毕
+                    </p>
+                  </div>
+                </Timeline.Item>
+                {/* <Timeline.Item style={{ paddingBottom: 10 }}>
+                  <span className={styles.time} style={{ color: '#fff' }}>
+                    10:00:00
+                  </span>
+                  <div>
+                    <span className={styles.status} style={{ color: '#fff' }}>
+                      处理
+                    </span>
+                  </div>
+                  <div>
+                    <p className={styles.content} style={{ color: '#fff' }}>
+                      上报该火警为真实火警
+                    </p>
+                    <p className={styles.contact} style={{ color: '#4f6793' }}>
+                      <span>安全员：张三</span>
+                      <span className={styles.phone}>17777777777</span>
+                    </p>
+                  </div>
+                </Timeline.Item> */}
               </Timeline>
             </div>
           </Row>
-          <div className={styles.bottom}>
-            <div className={styles.arrowRight} />
-            <div className={styles.picMain} />
-            <div className={styles.arrowLeft} />
-          </div>
+          <Row>
+            <div className={styles.bottom}>
+              <Col span={2}>
+                <div
+                  className={styles.arrowLeft}
+                  style={{ backgroundImage: `url(${arrowLeft})` }}
+                />
+              </Col>
+              <Col span={20}>
+                <div className={styles.picMain}>
+                  <div className={styles.fireimg} style={{ backgroundImage: `url(${imgBg})` }} />
+
+                  <div className={styles.fireimg2} style={{ backgroundImage: `url(${imgBg})` }} />
+
+                  <div className={styles.fireimg3} style={{ backgroundImage: `url(${imgBg})` }} />
+                </div>
+              </Col>
+              <Col span={2}>
+                <div
+                  className={styles.arrowRight}
+                  style={{ backgroundImage: `url(${arrowRight})` }}
+                />
+              </Col>
+            </div>
+          </Row>
         </section>
       </FcSection>
     );
