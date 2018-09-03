29a954a9e051e2e11f1b52d091fba3bbc8c855b1
wangyun <wangyunsxz@qq.com>
2018-09-03 11:25:41 +0800

    feat: !32 feature 219, 206
    Merge pull request !32 from yulinzkg/feat/safety-gov

---
+++ src/pages/BigPlatform/Safety/Components/MapTypeBar.js
@@ -0,0 +1,81 @@
+import React, { Component } from 'react';
+import classNames from 'classnames';
+import styles from './MapTypeBar.less';
+
+const typeList = [
+  {
+    id: 'satellite',
+    name: '卫星',
+    icon: 'iconSatellite',
+  },
+  {
+    id: 'road',
+    name: '路况',
+    icon: 'iconRoad',
+  },
+];
+class MapTypeBar extends Component {
+  state = {
+    active: {
+      satellite: false,
+      road: false,
+    },
+  };
+  componentDidMount() {}
+
+  componentWillUnmount() {}
+
+  handleClick = id => {
+    const { active } = this.state;
+    const { __map__: map } = this.props;
+    const layersArr = [new window.AMap.TileLayer.Satellite(), new window.AMap.TileLayer.RoadNet()];
+    active[id] = !active[id];
+
+    this.setState({
+      active,
+    });
+    const layers = [new window.AMap.TileLayer()];
+    typeList.forEach((item, index) => {
+      if (active[item.id]) {
+        layers.push(layersArr[index]);
+      }
+    });
+    map.setLayers(layers);
+  };
+
+  render() {
+    const { style = {}, ...restProps } = this.props;
+    const { active } = this.state;
+    return (
+      <div className={styles.typeLsit} style={style} {...restProps}>
+        {typeList.map(item => {
+          const itemStyles = classNames(styles.typeItem, {
+            [styles.active]: active[item.id],
+          });
+          return (
+            <div
+              key={item.id}
+              className={itemStyles}
+              onClick={() => {
+                this.handleClick(item.id);
+              }}
+            >
+              <span className={styles[item.icon]} />
+              <span className={styles.itemName}>{item.name}</span>
+            </div>
+          );
+        })}
+        {/* <div className={itemStyles} onClick={()=>{this.handleClick()}}>
+          <span className={styles.iconSatellite} />
+          <span className={styles.itemName}>卫星</span>
+        </div>
+        <div className={itemStyles} onClick={()=>{this.handleClick()}}>
+          <span className={styles.iconRoad} />
+          <span className={styles.itemName}>路况</span>
+        </div> */}
+      </div>
+    );
+  }
+}
+
+export default MapTypeBar;
