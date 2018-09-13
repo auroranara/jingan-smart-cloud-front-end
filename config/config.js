090e751552ce51e9852cb66b7672a4229d04cb74
wangyun <ywang@jingan-china.net>
2018-09-13 16:11:48 +0800

    fix: 修改废弃监测时间错乱

--- config/config.js
+++ config/config.js
@@ -33,7 +33,7 @@ export default {
       pathRewrite: { '^/mock': '/mock' },
     },
     '/gsafe': {
-      target: `http://${hosts.dev}`,
+      target: `http://${hosts.test}`,
       changeOrigin: true,
       pathRewrite: { '^/gsafe': '/gsafe' },
     },
