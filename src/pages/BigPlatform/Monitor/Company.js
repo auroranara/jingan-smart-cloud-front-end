329fffc870ec672c2370c7d7131c0bd1f581bd47
luhua <hlu@jingan-china.net>
2018-09-13 15:21:51 +0800

    fix(fire-control): fix some

--- src/pages/BigPlatform/Monitor/Company.js
+++ src/pages/BigPlatform/Monitor/Company.js
@@ -290,8 +290,8 @@ export default class App extends PureComponent {
           </Row>
         </div>
         <VideoPlay
-          dispatch={dispatch}
-          actionType="monitor/fetchStartToPlay"
+          // dispatch={dispatch}
+          // actionType="monitor/fetchStartToPlay"
           videoList={allCamera}
           visible={videoVisible}
           keyId={videoKeyId} // keyId
