706695ccaf628c3f9be3d49b0966a5ed25a29592
panchengyan <cypan@jingan-china.net>
2018-09-14 09:22:36 +0800

    feat(monitor): 废气改为暂未接入

--- src/pages/BigPlatform/Monitor/sections/ExhaustMonitor.js
+++ src/pages/BigPlatform/Monitor/sections/ExhaustMonitor.js
@@ -7,6 +7,7 @@ import ExhaustCards from '../components/ExhaustCards';
 import ExSection from './ExSection';
 
 import timeIcon from '../imgs/timeIcon.png';
+import waterBg from '../imgs/waterBg.png';
 
 const Option = Select.Option;
 
@@ -25,61 +26,55 @@ export default class ExhaustMonitor extends PureComponent {
     }, 5 * 60 * 1000);
   }
 
-  cards = (
-    <Row gutter={12} style={{ margin: 0, height: '100%' }}>
-      <Col style={{ height: '100%' }} span={6}>
-        <ExhaustCards num="8.91" unit="mg/m³" title="SO₂" />
-      </Col>
-      <Col style={{ height: '100%' }} span={6}>
-        <ExhaustCards num="8.91" unit="mg/m³" title="NOX" />
-      </Col>
-      <Col style={{ height: '100%' }} span={6}>
-        <ExhaustCards num="8.91" unit="mg/m³" title="颗粒物" />
-      </Col>
-      <Col style={{ height: '100%' }} span={6}>
-        <ExhaustCards num="8.91" unit="m/s" title="流速" />
-      </Col>
-      <Col style={{ height: '100%' }} span={6}>
-        <ExhaustCards num="8.91" unit="mg/m³" title="SO₂折算" />
-      </Col>
-      <Col style={{ height: '100%' }} span={6}>
-        <ExhaustCards num="8.91" unit="mg/m³" title="NOX折算" />
-      </Col>
-      <Col style={{ height: '100%' }} span={6}>
-        <ExhaustCards num="8.91" unit="mg/m³" title="颗粒物折算" />
-      </Col>
-      <Col style={{ height: '100%' }} span={6}>
-        <ExhaustCards num="8.91" unit="℃" title="温度" />
-      </Col>
-      <Col style={{ height: '100%' }} span={6}>
-        <ExhaustCards num="8.91" unit="MPa" title="压力" />
-      </Col>
-      <Col style={{ height: '100%' }} span={6}>
-        <ExhaustCards num="8.91" unit="%" title="氧浓度" />
-      </Col>
-    </Row>
-  );
-
   render() {
+    const list = [];
+
     return (
       <ExSection title="废气监测">
-        <section className={styles.container}>
-          <span className={styles.selectIcon}>
-            <Select defaultValue="厂区：一车间" dropdownClassName={styles.selectDropDown}>
-              <Option value="one">厂区：一车间</Option>
-            </Select>
-          </span>
-          <Row span={24} style={{ height: '12%' }}>
-            <Col span={24} style={{ height: '100%' }}>
-              <div className={styles.timeSection}>
-                <span className={styles.timeIcon} style={{ backgroundImage: `url(${timeIcon})` }} />
-                <span className={styles.day}>{this.state.day}</span>
-                <span className={styles.min}>{this.state.time}</span>
+        {list && list.length ? (
+          <section className={styles.container}>
+            <span className={styles.selectIcon}>
+              <Select defaultValue="厂区：一车间" dropdownClassName={styles.selectDropDown}>
+                <Option value="one">厂区：一车间</Option>
+              </Select>
+            </span>
+            <Row span={24} style={{ height: '12%' }}>
+              <Col span={24} style={{ height: '100%' }}>
+                <div className={styles.timeSection}>
+                  <span
+                    className={styles.timeIcon}
+                    style={{ backgroundImage: `url(${timeIcon})` }}
+                  />
+                  <span className={styles.day}>{this.state.day}</span>
+                  <span className={styles.min}>{this.state.time}</span>
+                </div>
+              </Col>
+            </Row>
+            {list.map(i => (
+              <div key={i} className={styles.oneCards}>
+                <Row gutter={14} style={{ margin: 0, height: '100%' }}>
+                  {[0, 1, 2, 3].map(() => {
+                    return (
+                      <Col style={{ height: '100%' }} span={6}>
+                        <ExhaustCards num="8.91" unit="%" title="氧浓度" />
+                      </Col>
+                    );
+                  })}
+                </Row>
               </div>
-            </Col>
-          </Row>
-          <div className={styles.oneCards}>{this.cards}</div>
-        </section>
+            ))}
+          </section>
+        ) : (
+          <div
+            className={styles.noCards}
+            style={{
+              background: `url(${waterBg})`,
+              backgroundRepeat: 'no-repeat',
+              backgroundPosition: 'center center',
+              backgroundSize: '40% 55%',
+            }}
+          />
+        )}
       </ExSection>
     );
   }
