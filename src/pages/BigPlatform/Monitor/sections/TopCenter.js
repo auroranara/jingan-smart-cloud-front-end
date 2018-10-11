9c36dcbf3a0434c0ed65b34330d1c1fa65aca79a
wangyun <ywang@jingan-china.net>
2018-10-11 20:14:48 +0800

    fix: score为0时显示水波图

--- src/pages/BigPlatform/Monitor/sections/TopCenter.js
+++ src/pages/BigPlatform/Monitor/sections/TopCenter.js
@@ -1,22 +1,22 @@
 import React, { PureComponent } from 'react';
-import { Col, Modal, Table, Pagination } from 'antd'
+import { Col, Modal, Table, Pagination } from 'antd';
 import WaterWave from 'components/Charts/ScoreWaterWave';
-import styles from './TopCenter.less'
-import { connect } from 'dva'
+import styles from './TopCenter.less';
+import { connect } from 'dva';
 // import classNames from 'classnames';
 
-import abnormalDevice from '../../../../assets/abnormal-device.png'
-import deviceTotalNumber from '../../../../assets/device-total-number.png'
-import missingDevice from '../../../../assets/missing-device.png'
-import fireHost from '../imgs/fire-host.png'
-import arrow from '../imgs/arrow.png'
-import redCircle from '../imgs/redCircle.png'
-import blueCircle from '../imgs/blueCircle.png'
-import SignalAnime from '../Components/SignalAnime'
+import abnormalDevice from '../../../../assets/abnormal-device.png';
+import deviceTotalNumber from '../../../../assets/device-total-number.png';
+import missingDevice from '../../../../assets/missing-device.png';
+import fireHost from '../imgs/fire-host.png';
+import arrow from '../imgs/arrow.png';
+import redCircle from '../imgs/redCircle.png';
+import blueCircle from '../imgs/blueCircle.png';
+import SignalAnime from '../Components/SignalAnime';
 import Ellipsis from '@/components/Ellipsis';
 import moment from 'moment';
-const hDivider = 'http://data.jingan-china.cn/v2/big-platform/fire-control/com/split_h.png'
-const divider = 'http://data.jingan-china.cn/v2/big-platform/fire-control/com/split.png'
+const hDivider = 'http://data.jingan-china.cn/v2/big-platform/fire-control/com/split_h.png';
+const divider = 'http://data.jingan-china.cn/v2/big-platform/fire-control/com/split.png';
 
 const alarmColumns = [
   {
@@ -38,22 +38,22 @@ const alarmColumns = [
     key: 'unormalParams',
     dataIndex: 'unormalParams',
     align: 'center',
-    render: (text, record) => (
-      <Ellipsis lines={1}>
-        {text}
-      </Ellipsis>
-    ),
+    render: (text, record) => <Ellipsis lines={1}>{text}</Ellipsis>,
   },
   {
     title: '区域位置',
     key: 'areaLocation',
     align: 'center',
     render: (text, record) => (
-      <span>{record.area}{record.area && record.location && '：'}{record.location}</span>
+      <span>
+        {record.area}
+        {record.area && record.location && '：'}
+        {record.location}
+      </span>
     ),
     width: 190,
   },
-]
+];
 const missingColumns = [
   {
     title: '设备号',
@@ -66,7 +66,11 @@ const missingColumns = [
     key: 'areaLocation',
     align: 'center',
     render: (text, record) => (
-      <span>{record.area}{record.area && record.location && '：'}{record.location}</span>
+      <span>
+        {record.area}
+        {record.area && record.location && '：'}
+        {record.location}
+      </span>
     ),
   },
   {
@@ -74,66 +78,65 @@ const missingColumns = [
     key: 'statusTime',
     dataIndex: 'statusTime',
     align: 'center',
-    render: (text) => (
-      <span>{text ? moment(text).format('YYYY-MM-DD hh:mm') : '暂无数据'}</span>
-    ),
+    render: text => <span>{text ? moment(text).format('YYYY-MM-DD hh:mm') : '暂无数据'}</span>,
   },
-]
+];
 
 @connect(({ monitor, errorDevicesLoading, loading }) => ({
   monitor,
   errorDevicesLoading: loading.effects['monitor/fetchErrorDevices'],
 }))
 export default class TopCenter extends PureComponent {
-
   state = {
     modalTitle: '',
     modalVisible: false,
-  }
-
+  };
 
   // 打开弹窗
-  handleViewModal = (title) => {
-    const { fetchErrorDevices } = this.props
-    fetchErrorDevices(title === "报警设备" ? 2 : 0)
+  handleViewModal = title => {
+    const { fetchErrorDevices } = this.props;
+    fetchErrorDevices(title === '报警设备' ? 2 : 0);
     this.setState({
       modalTitle: title,
       modalVisible: true,
-    })
-  }
+    });
+  };
 
   // 关闭弹窗
   handleModalClose = () => {
     this.setState({
       modalVisible: false,
-    })
-  }
+    });
+  };
 
   // 处理分页
   handlePageChange = (pageNum, pageSize) => {
-    const { dispatch } = this.props
+    const { dispatch } = this.props;
     dispatch({
       type: 'monitor/handleDevicesPagination',
       payload: { pageNum },
-    })
-  }
+    });
+  };
 
   handleAfterClose = () => {
-    const { dispatch } = this.props
+    const { dispatch } = this.props;
     dispatch({
       type: 'monitor/saveErrorDevices',
       payload: [],
-    })
-  }
+    });
+  };
 
   // 渲染报警设备、失联设备弹窗
   renderModal = () => {
-    const { modalTitle, modalVisible } = this.state
-    const { errorDevice: { errorDevicesByPage, pageNum, total, pageSize }, errorDevicesLoading } = this.props
-    const pages = total % pageSize === 0 ? total / pageSize : (Math.round(total / pageSize) + 1)
+    const { modalTitle, modalVisible } = this.state;
+    const {
+      errorDevice: { errorDevicesByPage, pageNum, total, pageSize },
+      errorDevicesLoading,
+    } = this.props;
+    const pages = total % pageSize === 0 ? total / pageSize : Math.round(total / pageSize) + 1;
     const title = (
       <div className={styles.modalTitle}>
-        <div className={styles.sectionTitleIcon}></div>
+        <div className={styles.sectionTitleIcon} />
         <span>{modalTitle}</span>
         <div className={styles.sectionTotal}>
           <span>共计</span>
@@ -141,7 +144,7 @@ export default class TopCenter extends PureComponent {
           <span>个</span>
         </div>
       </div>
-    )
+    );
 
     return (
       <Modal
@@ -161,43 +164,53 @@ export default class TopCenter extends PureComponent {
           pagination={false}
         />
         <div className={styles.footer}>
-          <div><span className={styles.pagesText}>每页{pageSize}行，共计{pages}页</span></div>
-          <Pagination
-            current={pageNum}
-            total={total}
-            onChange={this.handlePageChange}
-          />
+          <div>
+            <span className={styles.pagesText}>
+              每页
+              {pageSize}
+              行，共计
+              {pages}页
+            </span>
+          </div>
+          <Pagination current={pageNum} total={total} onChange={this.handlePageChange} />
         </div>
       </Modal>
-    )
-  }
+    );
+  };
 
   // 渲染分割线
-  renderDivider = (src) => {
+  renderDivider = src => {
     return (
-      <div className={styles.divider} style={{
-        backgroundImage: `url(${src})`,
-        backgroundRepeat: 'no-repeat',
-        backgroundPosition: 'center center',
-        backgroundSize: '100% 100%',
-      }}>
-      </div>
-    )
-  }
+      <div
+        className={styles.divider}
+        style={{
+          backgroundImage: `url(${src})`,
+          backgroundRepeat: 'no-repeat',
+          backgroundPosition: 'center center',
+          backgroundSize: '100% 100%',
+        }}
+      />
+    );
+  };
 
   // 渲染报警设备和失联设备
   renderMonitor = (src, title, number, color) => {
-
     return (
-      <div className={styles.section} onClick={() => { this.handleViewModal(title) }}>
-        <div className={styles.leftImg}
+      <div
+        className={styles.section}
+        onClick={() => {
+          this.handleViewModal(title);
+        }}
+      >
+        <div
+          className={styles.leftImg}
           style={{
             backgroundImage: `url(${src})`,
             backgroundRepeat: 'no-repeat',
             backgroundPosition: 'center center',
             backgroundSize: '68% 75%',
-          }}>
-        </div>
+          }}
+        />
         <div className={styles.rightContent}>
           <div className={styles.title}>{title}</div>
           <div className={styles.number}>
@@ -205,12 +218,14 @@ export default class TopCenter extends PureComponent {
           </div>
         </div>
       </div>
-    )
-  }
+    );
+  };
 
   render() {
-    const { countAndExponent: { score, count, outContact, unnormal }, fireAlarmSystem: { fire_state, fault_state } } = this.props
-
+    const {
+      countAndExponent: { score, count, outContact, unnormal },
+      fireAlarmSystem: { fire_state, fault_state },
+    } = this.props;
 
     return (
       <Col span={13} style={{ height: '100%' }} className={styles.topCenter}>
@@ -221,28 +236,34 @@ export default class TopCenter extends PureComponent {
               <div className={styles.topTitle}>监测指数</div>
               <div className={styles.monitorContent}>
                 <div className={styles.waterWave}>
-                  {score && (
-                    < WaterWave
-                      color={((!!unnormal || score < 80) && "#FF5256") || "#0082FD"}
+                  {(score || score === 0) && (
+                    <WaterWave
+                      color={((!!unnormal || score < 80) && '#FF5256') || '#0082FD'}
                       percentColor="white"
                       percentFontSize="48px"
                       percent={score}
                       isNumber={true}
                       height={105}
-                    />)}
+                    />
+                  )}
                 </div>
                 {this.renderDivider(hDivider)}
                 <div className={styles.totalNum}>
-                  <div className={styles.totalImg}
+                  <div
+                    className={styles.totalImg}
                     style={{
                       backgroundImage: `url(${deviceTotalNumber})`,
                       backgroundRepeat: 'no-repeat',
                       groundPosition: 'center center',
                       backgroundSize: '100% 100%',
                     }}
-                  ></div>
-                  <div className={styles.title}><span className={styles.text}>设备总数</span></div>
-                  <div className={styles.number}><span className={styles.text}>{count}</span></div>
+                  />
+                  <div className={styles.title}>
+                    <span className={styles.text}>设备总数</span>
+                  </div>
+                  <div className={styles.number}>
+                    <span className={styles.text}>{count}</span>
+                  </div>
                 </div>
                 {this.renderDivider(hDivider)}
                 <div className={styles.errorDevice}>
@@ -257,40 +278,43 @@ export default class TopCenter extends PureComponent {
         {/* 火灾自动报警监测 */}
         <Col span={12} style={{ height: '100%', padding: '0 6px' }}>
           <div className={styles.sectionMain}>
-            < div className={styles.shadowIn}>
+            <div className={styles.shadowIn}>
               <div className={styles.topTitle}>
-                <div className={styles.sectionTitleIcon}></div>
+                <div className={styles.sectionTitleIcon} />
                 火灾自动报警监测
-                      </div>
+              </div>
               <div className={styles.monitorContent}>
                 <div className={styles.iconContainer}>
-                  <div className={styles.fireHost}
+                  <div
+                    className={styles.fireHost}
                     style={{
                       backgroundImage: `url(${fireHost})`,
                       backgroundRepeat: 'no-repeat',
                       groundPosition: 'center center',
                       backgroundSize: '100% 100%',
-                    }}>
-                  </div>
+                    }}
+                  />
                   <span className={styles.span}>（消防主机）</span>
                   <div className={styles.anime}>
                     <SignalAnime />
                   </div>
                 </div>
                 <div className={styles.arrContainer}>
-                  <div className={styles.arrow}
+                  <div
+                    className={styles.arrow}
                     style={{
                       backgroundImage: `url(${arrow})`,
                       backgroundRepeat: 'no-repeat',
                       groundPosition: 'center center',
                       backgroundSize: '100% 100%',
-                    }}>
-                  </div>
+                    }}
+                  />
                   <span className={styles.fireText}>监测火警数</span>
                   <span className={styles.errorText}>监测故障数</span>
                 </div>
                 <div className={styles.numberContainer}>
-                  <div className={styles.circle}
+                  <div
+                    className={styles.circle}
                     style={{
                       backgroundImage: `url(${redCircle})`,
                       backgroundRepeat: 'no-repeat',
@@ -298,9 +322,12 @@ export default class TopCenter extends PureComponent {
                       backgroundSize: '100% 100%',
                     }}
                   >
-                    <span style={{ color: '#E46867' }} className={styles.number}>{fire_state}</span>
+                    <span style={{ color: '#E46867' }} className={styles.number}>
+                      {fire_state}
+                    </span>
                   </div>
-                  <div className={styles.circle}
+                  <div
+                    className={styles.circle}
                     style={{
                       backgroundImage: `url(${blueCircle})`,
                       backgroundRepeat: 'no-repeat',
@@ -308,7 +335,9 @@ export default class TopCenter extends PureComponent {
                       backgroundSize: '100% 100%',
                     }}
                   >
-                    <span style={{ color: '#07A4F9' }} className={styles.number}>{fault_state}</span>
+                    <span style={{ color: '#07A4F9' }} className={styles.number}>
+                      {fault_state}
+                    </span>
                   </div>
                 </div>
               </div>
@@ -317,6 +346,6 @@ export default class TopCenter extends PureComponent {
         </Col>
         {this.renderModal()}
       </Col>
-    )
+    );
   }
 }
