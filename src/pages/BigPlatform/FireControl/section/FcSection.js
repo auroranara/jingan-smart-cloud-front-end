0df1c5c7883345981bad8d2d2ce5e17a10492a8e
wangyun <ywang@jingan-china.net>
2018-11-12 14:02:34 +0800

    fix: backface增加浏览器版本

--- src/pages/BigPlatform/FireControl/section/FcSection.js
+++ src/pages/BigPlatform/FireControl/section/FcSection.js
@@ -5,29 +5,42 @@ import styles from './FcSection.less';
 
 export default class FcSection extends PureComponent {
   render() {
-    const { title, backTitle, handleBack=null, children=null, style={}, isBack=false, ...restProps } = this.props;
+    const {
+      title,
+      backTitle,
+      handleBack = null,
+      children = null,
+      style = {},
+      isBack = false,
+      ...restProps
+    } = this.props;
     const newStyle = {
       padding: '0 15px',
       overflow: 'hidden',
       height: '100%',
       backfaceVisibility: 'hidden',
+      WebkitBackfaceVisibility: 'hidden',
       boxShadow: '0 0 1.1em rgba(9, 103, 211, 0.9) inset',
       background: 'rgba(9,103,211,0.1)',
       transform: isBack ? 'rotateY(180deg)' : 'translateY(-100%)',
       ...style,
-    }
+    };
 
     return (
       <div style={newStyle} {...restProps}>
-        {title
-          ? (
-            <h3 className={styles.title}>
-              <span className={styles.dot}></span>
-              {title}
-              {backTitle && <span className={styles.back} onClick={handleBack}>{backTitle}<Icon type="double-right" style={{ marginLeft: 3 }} /></span>}
-            </h3>
-          ) : null}
-         {children}
+        {title ? (
+          <h3 className={styles.title}>
+            <span className={styles.dot} />
+            {title}
+            {backTitle && (
+              <span className={styles.back} onClick={handleBack}>
+                {backTitle}
+                <Icon type="double-right" style={{ marginLeft: 3 }} />
+              </span>
+            )}
+          </h3>
+        ) : null}
+        {children}
       </div>
     );
   }
