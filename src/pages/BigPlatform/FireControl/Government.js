cb9281039d50a5a2f279f37e87dfaeec8a8d6d17
luhua <hlu@jingan-china.net>
2018-11-12 17:23:57 +0800

    fix(big-fire): add business type

--- src/pages/BigPlatform/FireControl/Government.js
+++ src/pages/BigPlatform/FireControl/Government.js
@@ -110,13 +110,13 @@ export default class FireControlBigPlatform extends PureComponent {
     const gridId = this.getGridId();
 
     dispatch({ type: 'bigFireControl/fetchOvAlarmCounts', payload: { gridId } });
-    dispatch({ type: 'bigFireControl/fetchOvDangerCounts', payload: { gridId } });
+    dispatch({ type: 'bigFireControl/fetchOvDangerCounts', payload: { gridId, businessType: 2 } });
     dispatch({ type: 'bigFireControl/fetchSys', payload: { gridId } });
     dispatch({ type: 'bigFireControl/fetchAlarm', payload: { gridId } });
     dispatch({ type: 'bigFireControl/fetchAlarmHistory', payload: { gridId } });
     dispatch({ type: 'bigFireControl/fetchFireTrend', payload: { gridId } });
     dispatch({ type: 'bigFireControl/fetchCompanyFireInfo', payload: { gridId } });
-    dispatch({ type: 'bigFireControl/fetchDanger', payload: { gridId } });
+    dispatch({ type: 'bigFireControl/fetchDanger', payload: { gridId, businessType: 2 } });
 
     this.fetchInitLookUp();
 
@@ -375,10 +375,10 @@ export default class FireControlBigPlatform extends PureComponent {
 
     const { id, isFire, latitude, longitude } = item;
     dispatch({ type: 'bigFireControl/fetchOvAlarmCounts', payload: { companyId: id, gridId } });
-    dispatch({ type: 'bigFireControl/fetchOvDangerCounts', payload: { company_id: id, gridId } });
+    dispatch({ type: 'bigFireControl/fetchOvDangerCounts', payload: { company_id: id, gridId, businessType: 2 } });
     dispatch({ type: 'bigFireControl/fetchCompanyOv', payload: { company_id: id, gridId } });
     dispatch({ type: 'bigFireControl/fetchFireTrend', payload: { companyId: id, gridId } });
-    dispatch({ type: 'bigFireControl/fetchDanger', payload: { company_id: id, gridId } });
+    dispatch({ type: 'bigFireControl/fetchDanger', payload: { company_id: id, gridId, businessType: 2 } });
 
     // 点击火警或地图中的企业时，获取视频相关信息
     this.handleVideoSelect(id);
