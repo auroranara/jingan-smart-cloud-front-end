3dafb3e77ef06ec66f39b437b13200d44cd2ea7e
luhua <hlu@jingan-china.net>
2018-11-13 15:43:25 +0800

    fix(dashboard): fix bug #1686

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -15,6 +15,9 @@ const safeItem = { src: safe, url: '', label: '安全驾驶舱' };
 const fireItem = { src: fire, url: '', label: '消防驾驶舱' };
 const monitorItem = { src: monitor, url: '', label: '动态监测驾驶舱' }
 
+// 1=>安全生产 2=>消防 3=>环保(暂时对应动态监测大屏)
+const CLASSIFICATION = { safety: 1, fireControl: 2, dynamicMonitor: 3 };
+
 @connect(({ user }) => ({
   user,
 }))
@@ -33,33 +36,87 @@ export default class Dashboard extends PureComponent {
           companyBasicInfo: { fireService, safetyProduction, monitorService } = {},
           unitType,
           companyId,
+          regulatoryClassification,
         },
       },
     } = this.props;
 
-    const [safetyAuth, fireControlAuth, dynamicMonitorAuth] = Object.entries(codes.dashboard).map(([k, v]) => permissionCodes.includes(v));
-    // console.log(safetyAuth, fireControlAuth, dynamicMonitorAuth);
+    const classification = Array.isArray(regulatoryClassification) && regulatoryClassification.map(n => Number.parseInt(n, 10)) || [];
+    const [[safetyAuth, clfcSafetyAuth], [fireControlAuth, clfcFireControlAuth], [dynamicMonitorAuth, clfcDynamicMonitorAuth]] = Object.entries(codes.dashboard)
+      .map(([k, v]) => [permissionCodes.includes(v), classification.includes(CLASSIFICATION[k])]);
+    console.log([safetyAuth, clfcSafetyAuth], [fireControlAuth, clfcFireControlAuth], [dynamicMonitorAuth, clfcDynamicMonitorAuth]);
 
     safeItem.url = `${window.publicPath}#/big-platform/safety/government`
     fireItem.url = `${window.publicPath}#/big-platform/fire-control/government/index`
     // unitType  1：维保企业 2：政府 3：运营 4:企事业主体
     // 政府根据companyBasicInfo的数据来
-    if (unitType === 2) {
-      //TODO 政府大屏开启
-      this.setState({ safetyProduction: safetyProduction && safetyAuth, fireService: fireService && fireControlAuth });
-    } else if (unitType === 3) {
-      // 运营可以看所有政府大屏
-      this.setState({ safetyProduction: 1 && safetyAuth, fireService: 1 && fireControlAuth });
-    } else {
-      // 企业根据companyBasicInfo的数据来
-      safeItem.url = `${window.publicPath}#/big-platform/safety/company/${companyId}`;
-      fireItem.url = `${window.publicPath}#/big-platform/fire-control/company/${companyId}`;
-      monitorItem.url = `${window.publicPath}#/big-platform/monitor/company/${companyId}`
-      this.setState({
-        safetyProduction: safetyProduction && safetyAuth,
-        fireService: unitType === 1 ? 0 : fireService && fireControlAuth, // 这个迭代维保企业不能看消防
-        monitorService: monitorService && dynamicMonitorAuth,
-      });
+    // if (unitType === 2) {
+    //   //TODO 政府大屏开启
+    //   this.setState({ safetyProduction: safetyProduction && safetyAuth, fireService: fireService && fireControlAuth });
+    // } else if (unitType === 3) {
+    //   // 运营可以看所有政府大屏
+    //   this.setState({ safetyProduction: 1 && safetyAuth, fireService: 1 && fireControlAuth });
+    // } else {
+    //   // 企业根据companyBasicInfo的数据来
+    //   safeItem.url = `${window.publicPath}#/big-platform/safety/company/${companyId}`;
+    //   fireItem.url = `${window.publicPath}#/big-platform/fire-control/company/${companyId}`;
+    //   monitorItem.url = `${window.publicPath}#/big-platform/monitor/company/${companyId}`
+    //   this.setState({
+    //     safetyProduction: safetyProduction && safetyAuth,
+    //     fireService: unitType === 1 ? 0 : fireService && fireControlAuth, // 这个迭代维保企业不能看消防
+    //     monitorService: monitorService && dynamicMonitorAuth,
+    //   });
+    // }
+
+    const safeUrl = `${window.publicPath}#/big-platform/safety/company/${companyId}`;
+    const fireUrl = `${window.publicPath}#/big-platform/fire-control/company/${companyId}`;
+    const monitorUrl = `${window.publicPath}#/big-platform/monitor/company/${companyId}`;
+
+    // 企事业主体和政府有业务分类，维保和运营没有
+    // 所以企事业主体和政府的大屏权限 = 用户业务权限 && 企事业业务分类 && 账户被配置的权限，运营和维保企业的大屏权限 = 用户业务权限 && 账户被配置的权限
+    switch(unitType) {
+      // 维保企业
+      case 1:
+        safeItem.url = safeUrl;
+        fireItem.url = fireUrl;
+        monitorItem.url = monitorUrl;
+        this.setState({
+          safetyProduction: safetyProduction && safetyAuth,
+          fireService: 0, // 这个迭代维保企业不能看消防
+          monitorService: monitorService && dynamicMonitorAuth,
+        });
+        break;
+
+      // 政府
+      case 2:
+        this.setState({
+          safetyProduction: safetyProduction && safetyAuth && clfcSafetyAuth,
+          fireService: fireService && fireControlAuth && clfcFireControlAuth,
+        });
+        break;
+
+      // 运营
+      case 3:
+        this.setState({
+          safetyProduction: 1 && safetyAuth,
+          fireService: 1 && fireControlAuth,
+        });
+        break;
+
+      // 企事业
+      case 4:
+        safeItem.url = safeUrl;
+        fireItem.url = fireUrl;
+        monitorItem.url = monitorUrl;
+        this.setState({
+          safetyProduction: safetyProduction && safetyAuth && clfcSafetyAuth,
+          fireService: fireService && fireControlAuth && clfcFireControlAuth,
+          monitorService: monitorService && dynamicMonitorAuth && clfcDynamicMonitorAuth,
+        });
+        break;
+
+      default:
+        console.warn(`unitType=${unitType} in Dashboard`);
     }
   }
 
@@ -72,16 +129,23 @@ export default class Dashboard extends PureComponent {
     //   (safetyProduction && !fireService && [safeItem]) ||
     //   (!safetyProduction && fireService && [fireItem]) ||
     //   [];
-    let imgWrapper = []
-    if (safetyProduction) {
-      imgWrapper.push(safeItem)
-    }
-    if (fireService) {
-      imgWrapper.push(fireItem)
-    }
-    if (monitorService) {
-      imgWrapper.push(monitorItem)
-    }
+
+    // let imgWrapper = [];
+    // if (safetyProduction) {
+    //   imgWrapper.push(safeItem)
+    // }
+    // if (fireService) {
+    //   imgWrapper.push(fireItem)
+    // }
+    // if (monitorService) {
+    //   imgWrapper.push(monitorItem)
+    // }
+
+    const items = [safeItem, fireItem, monitorItem];
+    const imgWrapper = [safetyProduction, fireService, monitorService].reduce((prev, next, i) => {
+      next && prev.push(items[i]);
+      return prev;
+    }, []);
 
     const goToBigScreen = url => {
       const win = window.open(url, '_blank');
