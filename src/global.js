96d1d4e1b63ad4f2339c44668d5a739a6d76e20c
wangyun <ywang@jingan-china.net>
2018-11-16 17:59:28 +0800

    build: 增加Performance监测

--- src/global.js
+++ src/global.js
@@ -1,6 +1,14 @@
 // import './polyfill';
 import 'moment/locale/zh-cn';
 import configs from '../config/project.config';
+// import Performance from 'web-report';
+
+// Performance({
+//   domain: 'http://192.168.14.59:7001/api/v1/report/web',
+//   add: {
+//     appId: '111376904C634F3BC778E8E9E22488DB',
+//   },
+// });
 
 const PROJECT_ENV = process.env.PROJECT_ENV || 'default';
 global.PROJECT_CONFIG = configs[PROJECT_ENV] || {};
