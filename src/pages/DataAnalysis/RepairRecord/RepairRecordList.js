dc449c1129cd0fa5d2dcfad2568c4792dcb39ccd
zangyunchong <13815207752@163.com>
2018-11-19 11:07:41 +0800

    fix: 一键报修记录维保子公司看不到维修单位

--- src/pages/DataAnalysis/RepairRecord/RepairRecordList.js
+++ src/pages/DataAnalysis/RepairRecord/RepairRecordList.js
@@ -26,8 +26,8 @@ const statusList = [
 ]
 
 // 栅格的配置
-const grdiSetting={
-  xl:8,md:12, sm:24, xs:24,
+const grdiSetting = {
+  xl: 8, md: 12, sm: 24, xs: 24,
 }
 
 @connect(({ dataAnalysis, user }) => ({
@@ -40,6 +40,90 @@ export default class RepairRecordList extends PureComponent {
     modalVisible: false,
     imageFiles: [], // 附件图片列表
     currentImage: 0,// 展示附件大图下标
+    scrollX: 1250, // 列表scroll宽度
+    columns: [
+      {
+        title: '工单编号',
+        dataIndex: 'work_order',
+        key: 'work_order',
+        align: 'center',
+        width: 240,
+      },
+      {
+        title: '报修单位',
+        dataIndex: 'company_name',
+        key: 'company_name',
+        align: 'center',
+        width: 350,
+      },
+      {
+        title: '报修人员',
+        dataIndex: 'createByName',
+        key: 'createByName',
+        align: 'center',
+        width: 120,
+      },
+      {
+        title: '联系电话',
+        dataIndex: 'createByPhone',
+        key: 'createByPhone',
+        align: 'center',
+        width: 140,
+      },
+      {
+        title: '报修时间',
+        dataIndex: 'create_date',
+        key: 'create_date',
+        align: 'center',
+        width: 200,
+        render: (val) => (
+          <span>{moment(+val).format('YYYY-MM-DD HH:mm:ss')}</span>
+        ),
+      },
+      {
+        title: '报修附件',
+        dataIndex: 'reportPhotos',
+        key: 'reportPhotos',
+        align: 'center',
+        width: 120,
+        render: (val, record) => {
+          return (
+            <Fragment>
+              {record.reportPhotos && record.reportPhotos.length ? (
+                <a onClick={() => this.handleShowModal(record.reportPhotos)}>
+                  查看附件
+                </a>
+              ) : (
+                  <span style={{ color: '#aaa' }}>查看附件</span>
+                )}
+            </Fragment>
+          )
+        },
+      },
+      {
+        title: '当前状态',
+        dataIndex: 'realStatus',
+        key: 'realStatus',
+        align: 'center',
+        width: 130,
+        render: (val) => (
+          <span style={{ color: `${(val === "已处理" && "#aaa") || (val === "待处理" && "red") || (val === "处理中" && "#1890FF")}` }}>{val}</span>
+        ),
+      },
+      {
+        title: '操作',
+        dataIndex: '操作',
+        key: '操作',
+        align: 'center',
+        fixed: 'right',
+        width: 120,
+        render: (val, record) => (
+          <span>
+            <a onClick={() => this.handleViewDetail(record.work_order)}>查看</a>
+          </span>
+        ),
+      },
+    ],
   }
   componentDidMount() {
     const {
@@ -51,8 +135,21 @@ export default class RepairRecordList extends PureComponent {
           },
         },
       },
+      user: {
+        currentUser: { companyBasicInfo: { isBranch } = {} },
+      },
     } = this.props
-
+    const { columns } = this.state;
+    if (!isBranch) {
+      columns.splice(6, 0, {
+        title: '维修单位',
+        dataIndex: 'unit_name',
+        key: 'unit_name',
+        align: 'center',
+        width: 350,
+      })
+      this.setState({ columns: [...columns], scrollX: 1600 });
+    }
     // 获取报修记录列表
     dispatch({
       type: 'dataAnalysis/fetchRepairRecords',
@@ -178,30 +275,32 @@ export default class RepairRecordList extends PureComponent {
       user: {
         currentUser: {
           unitType,
+          companyBasicInfo: { isBranch } = {},
         } = {},
       },
     } = this.props
     return (
       <Card>
         <Form layout="inline" className={styles.repairRecordForm}>
-            <Col {...grdiSetting}>
-              <FormItem className={styles.formItem}>
-                {getFieldDecorator('workOrder', {
-                  getValueFromEvent: e => e.target.value.trim(),
-                })(
-                  <Input placeholder="请输入工单编号"></Input>
-                )}
-              </FormItem>
-            </Col>
-            <Col  {...grdiSetting}>
-              <FormItem className={styles.formItem}>
-                {getFieldDecorator('companyName', {
-                  getValueFromEvent: e => e.target.value.trim(),
-                })(
-                  <Input placeholder="报修单位名称"></Input>
-                )}
-              </FormItem>
-            </Col>
+          <Col {...grdiSetting}>
+            <FormItem className={styles.formItem}>
+              {getFieldDecorator('workOrder', {
+                getValueFromEvent: e => e.target.value.trim(),
+              })(
+                <Input placeholder="请输入工单编号"></Input>
+              )}
+            </FormItem>
+          </Col>
+          <Col  {...grdiSetting}>
+            <FormItem className={styles.formItem}>
+              {getFieldDecorator('companyName', {
+                getValueFromEvent: e => e.target.value.trim(),
+              })(
+                <Input placeholder="报修单位名称"></Input>
+              )}
+            </FormItem>
+          </Col>
+          {!isBranch && (
             <Col  {...grdiSetting}>
               <FormItem className={styles.formItem}>
                 {getFieldDecorator('unitName', {
@@ -210,30 +309,30 @@ export default class RepairRecordList extends PureComponent {
                   <Input placeholder="维修单位名称"></Input>
                 )}
               </FormItem>
-            </Col>
-            <Col {...grdiSetting}>
-              <FormItem className={styles.formItem}>
-                {getFieldDecorator('status')(
-                  <Select placeholder="请选择维修状态">
-                    {statusList.map(({ value, label }) => unitType === 4 && value === 2 ? null : (<Option value={value} key={value} >{label}</Option>))}
-                  </Select>
-                )}
-              </FormItem>
-            </Col>
-            <Col {...grdiSetting}>
-              <FormItem className={styles.formItem} >
-                {getFieldDecorator('time')(
-                  <RangePicker
-                    style={{ width: '100%' }}
-                    format="YYYY-MM-DD HH:mm:ss"
-                    placeholder={['开始时间', '结束时间']}
-                    showTime={{
-                      defaultValue: [moment('0:0:0', 'HH:mm:ss'), moment('23:59:59', 'HH:mm:ss')],
-                    }}
-                  />
-                )}
-              </FormItem>
-            </Col>
+            </Col>)}
+          <Col {...grdiSetting}>
+            <FormItem className={styles.formItem}>
+              {getFieldDecorator('status')(
+                <Select placeholder="请选择维修状态">
+                  {statusList.map(({ value, label }) => unitType === 4 && value === 2 ? null : (<Option value={value} key={value} >{label}</Option>))}
+                </Select>
+              )}
+            </FormItem>
+          </Col>
+          <Col {...grdiSetting}>
+            <FormItem className={styles.formItem} >
+              {getFieldDecorator('time')(
+                <RangePicker
+                  style={{ width: '100%' }}
+                  format="YYYY-MM-DD HH:mm:ss"
+                  placeholder={['开始时间', '结束时间']}
+                  showTime={{
+                    defaultValue: [moment('0:0:0', 'HH:mm:ss'), moment('23:59:59', 'HH:mm:ss')],
+                  }}
+                />
+              )}
+            </FormItem>
+          </Col>
           <Col {...grdiSetting}>
             <FormItem>
               <Button onClick={this.handleQuery} type="primary">查询</Button>
@@ -248,7 +347,7 @@ export default class RepairRecordList extends PureComponent {
   }
 
   render() {
-    const { modalVisible, imageFiles, currentImage } = this.state
+    const { modalVisible, imageFiles, currentImage, columns, scrollX } = this.state
     const {
       dataAnalysis: {
         repairRecord: {
@@ -259,96 +358,6 @@ export default class RepairRecordList extends PureComponent {
         },
       },
     } = this.props
-    const columns = [
-      {
-        title: '工单编号',
-        dataIndex: 'work_order',
-        key: 'work_order',
-        align: 'center',
-        width: 240,
-      },
-      {
-        title: '报修单位',
-        dataIndex: 'company_name',
-        key: 'company_name',
-        align: 'center',
-        width: 350,
-      },
-      {
-        title: '报修人员',
-        dataIndex: 'createByName',
-        key: 'createByName',
-        align: 'center',
-        width: 120,
-      },
-      {
-        title: '联系电话',
-        dataIndex: 'createByPhone',
-        key: 'createByPhone',
-        align: 'center',
-        width: 140,
-      },
-      {
-        title: '报修时间',
-        dataIndex: 'create_date',
-        key: 'create_date',
-        align: 'center',
-        width: 200,
-        render: (val) => (
-          <span>{moment(+val).format('YYYY-MM-DD HH:mm:ss')}</span>
-        ),
-      },
-      {
-        title: '报修附件',
-        dataIndex: 'reportPhotos',
-        key: 'reportPhotos',
-        align: 'center',
-        width: 120,
-        render: (val, record) => {
-          return (
-            <Fragment>
-              {record.reportPhotos && record.reportPhotos.length ? (
-                <a onClick={() => this.handleShowModal(record.reportPhotos)}>
-                  查看附件
-                </a>
-              ) : (
-                  <span style={{ color: '#aaa' }}>查看附件</span>
-                )}
-            </Fragment>
-          )
-        },
-      },
-      {
-        title: '维修单位',
-        dataIndex: 'unit_name',
-        key: 'unit_name',
-        align: 'center',
-        width: 350,
-      },
-      {
-        title: '当前状态',
-        dataIndex: 'realStatus',
-        key: 'realStatus',
-        align: 'center',
-        width: 130,
-        render: (val) => (
-          <span style={{ color: `${(val === "已处理" && "#aaa") || (val === "待处理" && "red") || (val === "处理中" && "#1890FF")}` }}>{val}</span>
-        ),
-      },
-      {
-        title: '操作',
-        dataIndex: '操作',
-        key: '操作',
-        align: 'center',
-        fixed: 'right',
-        width: 120,
-        render: (val, record) => (
-          <span>
-            <a onClick={() => this.handleViewDetail(record.work_order)}>查看</a>
-          </span>
-        ),
-      },
-    ]
     return (
       <PageHeaderLayout
         title={title}
@@ -358,7 +367,7 @@ export default class RepairRecordList extends PureComponent {
         {this.renderForm()}
         {repairRecords && repairRecords.length ? (
           <Card className={styles.repairRecordList}>
-            <Table scroll={{ x: 1600 }} rowKey="id" columns={columns} dataSource={repairRecords} bordered pagination={false} />
+            <Table scroll={{ x: scrollX }} rowKey="id" columns={columns} dataSource={repairRecords} bordered pagination={false} />
             <Pagination
               style={{ marginTop: '20px', float: 'right' }}
               showQuickJumper
