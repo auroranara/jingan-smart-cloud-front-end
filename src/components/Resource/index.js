16a01b9a2088268048ed7cf3df93bd466312f9f5
wangyun <ywang@jingan-china.net>
2018-11-20 13:21:05 +0800

    feat: 增加video

--- src/components/Resource/index.js
+++ src/components/Resource/index.js
@@ -1,4 +1,6 @@
 import React, { PureComponent } from 'react';
+import { Player } from 'video-react';
+import 'node_modules/video-react/dist/video-react.css';
 
 const offices = ['pptx', 'docx', 'xlsx', 'ppt', 'doc', 'xls'];
 export default class Resource extends PureComponent {
@@ -25,8 +27,13 @@ export default class Resource extends PureComponent {
   };
 
   renderPdf = () => {
-    const { src, extension, key, styles } = this.props;
+    const { src, key, styles } = this.props;
     return <embed src={src} style={styles} type="application/pdf" />;
+  };
+
+  renderVideo = () => {
+    const { src, poster, key, styles } = this.props;
+    return <Player playsInline poster={poster} src={src} style={styles} />;
   };
 
   render() {
@@ -36,6 +43,8 @@ export default class Resource extends PureComponent {
       result = this.renderPdf();
     } else if (offices.indexOf(extension) > -1) {
       result = this.renderOffice();
+    } else if (extension === 'mp4') {
+      result = this.renderVideo();
     }
     return <div>{result}</div>;
   }
