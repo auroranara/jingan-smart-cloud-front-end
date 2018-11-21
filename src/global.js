ec361da79db3906f90963c9acbdf475c01c2cae1
wangyun <ywang@jingan-china.net>
2018-11-21 13:22:13 +0800

    feat: 修改上报地址

--- src/global.js
+++ src/global.js
@@ -1,14 +1,14 @@
 // import './polyfill';
 import 'moment/locale/zh-cn';
 import configs from '../config/project.config';
-// import Performance from 'web-report';
+import Performance from 'web-report';
 
-// Performance({
-//   domain: 'http://192.168.14.59:7001/api/v1/report/web',
-//   add: {
-//     appId: '111376904C634F3BC778E8E9E22488DB',
-//   },
-// });
+Performance({
+  domain: 'http://192.168.10.68:8001/api/v1/report/web',
+  add: {
+    appId: '79720FCD3AB10086CF5BE21BFE14C12C',
+  },
+});
 
 const PROJECT_ENV = process.env.PROJECT_ENV || 'default';
 global.PROJECT_CONFIG = configs[PROJECT_ENV] || {};
