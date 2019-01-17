2e8308fd120bb5fc64c429d370d5fc19b8114534
luhua <hlu@jingan-china.net>
2019-01-17 14:27:21 +0800

    feat(unit-safe): store temp

--- src/pages/BigPlatform/Safety/Company2/components/Components.js
+++ src/pages/BigPlatform/Safety/Company2/components/Components.js
@@ -1,5 +1,13 @@
 import DrawerContainer from './DrawerContainer';
 import DrawerSection from './DrawerSection';
 import Rect from './Rect';
+import Solar from './Solar';
 
-export { DrawerContainer, DrawerSection, Rect };
+export {
+  DrawerContainer,
+  DrawerSection,
+  Rect,
+  Solar,
+};
+
+export { ChartBar } from '@/pages/BigPlatform/NewFireControl/components/Components';
--- src/pages/BigPlatform/Safety/Company2/sections/IndexDrawer.js
+++ src/pages/BigPlatform/Safety/Company2/sections/IndexDrawer.js
@@ -1,33 +1,65 @@
 import React, { Fragment, PureComponent } from 'react';
 
 import styles from './IndexDrawer.less';
-import { DrawerContainer, DrawerSection, Rect } from '../components/Components';
+import {
+  ChartBar,
+  DrawerContainer,
+  DrawerSection,
+  Rect,
+  Solar,
+} from '../components/Components';
 
 const TYPE = 'index';
+const LABELS = ['安全巡查', '隐患排查', '动态监测', '安全档案'];
+const BAR_COLORS = ['85,134,244', '233,102,108', '244,185,85', '2,252,250'];
+const BAR_LIST = LABELS.map(label => ({ name: label, value: Math.floor(Math.random() * 100) }));
 
 export default class IndexDrawer extends PureComponent {
+  state={ selected: 0 };
+
   handleClose = () => {
     const { handleDrawerVisibleChange } = this.props;
     handleDrawerVisibleChange(TYPE);
   };
 
+  handleLabelChange = i => {
+    this.setState({ selected: i });
+  };
+
   render() {
     const { visible } = this.props;
+    const { selected } = this.state;
 
     const titleIcon = <Rect color='#0967d3' />;
 
     const left = (
       <Fragment>
         <DrawerSection title="构成">
-          index
+          <Solar />
         </DrawerSection>
         <DrawerSection title="分值">
-          bar
+          <ChartBar barWidth={30} barColors={BAR_COLORS} labelRotate={0} data={BAR_LIST} />
         </DrawerSection>
       </Fragment>
     );
 
-    const right = "cards";
+    const right = (
+      <div className={styles.right}>
+        <div className={styles.labels}>
+          {LABELS.map((label, i) => (
+            <span
+              className={i === selected ? styles.labelSelected : styles.label}
+              onClick={e => this.handleLabelChange(i)}
+            >
+              {label}
+            </span>
+          ))}
+        </div>
+        <p className={styles.desc}>共有两个点位超时未查</p>
+        <div className={styles.cards}>
+        </div>
+      </div>
+    );
 
     return (
       <DrawerContainer
