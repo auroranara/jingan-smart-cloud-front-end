7f86b740a60655094b4422717454d7da2df786b6
PanChengYan <cypan@jingan-china.net>
2019-01-25 15:55:27 +0800

    fix: fix status

--- src/pages/BigPlatform/Monitor/sections/EffluentMonitor.js
+++ src/pages/BigPlatform/Monitor/sections/EffluentMonitor.js
@@ -32,9 +32,10 @@ export default function EffluentMonitor(props) {
       waterRealTimeData: { status, updateTime, realTimeData = {}, unnormalCodes = [] },
     },
   } = props;
+  console.log('props', props);
 
   let rtData = realTimeData;
-  if (status === '0') rtData = {};
+  if (status === '-1') rtData = {};
 
   const handledParams = params.map(({ id, code, desc, unit }) => ({
     id,
@@ -43,9 +44,12 @@ export default function EffluentMonitor(props) {
     value: rtData[code],
     isBeyond: unnormalCodes.includes(code),
   }));
+  console.log('handledParams', handledParams);
 
   const sectionStyle = {
-    boxShadow: `0 0 1.1em rgba(${status === '2' ? '200,70,70' : '9,103,211'}, 0.9) inset`,
+    boxShadow: `0 0 1.1em rgba(${
+      status === '1' || status === '2' ? '200,70,70' : '9,103,211'
+    }, 0.9) inset`,
   };
 
   return (
