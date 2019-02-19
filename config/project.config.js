6e1e092ec383ff6dd7145f2c053c3a9f2166b744
sk <18921261761@163.com>
2019-02-19 09:56:41 +0800

    feat: 添加下载地址

--- config/project.config.js
+++ config/project.config.js
@@ -52,6 +52,8 @@ const configs = {
     projectShortName: '无锡晶安智慧云',
     code,
     layer: 'http://data.jingan-china.cn/v2/login/wuxi_download_layer.png',
+    ios: "itms-services://?action=download-manifest&url=https://www.jingan-china.cn/download/jxjd/official_version/JXJDInterSafe.plist&ran=",
+    android: "http://58.215.178.100:12083/acloud_new/v2/mobileVersion/version/getLatest?type=1&ran=",
   },
   xuzhou: {
     region: '徐州市',
@@ -62,6 +64,8 @@ const configs = {
       y: 34.501282,
       zoom: 14,
     },
+    ios: "itms-services://?action=download-manifest&url=https://www.jingan-china.cn/download/xz/official_version/XZInterSafe.plist&ran=",
+    android: "http://58.215.178.100:12081/acloud_new/v2/mobileVersion/version/getLatest?type=1&ran=",
   },
   limin: {
     region: '徐州市',
@@ -86,6 +90,8 @@ const configs = {
       'http://data.jingan-china.cn/v2/login/limin/2.png',
       'http://data.jingan-china.cn/v2/login/limin/3.png',
     ],
+    ios: "itms-services://?action=download-manifest&url=https://www.jingan-china.cn/download/xz/official_version/XZInterSafe.plist&ran=",
+    android: "http://58.215.178.100:12081/acloud_new/v2/mobileVersion/version/getLatest?type=1&ran=",
   },
   changshu: {
     region: '常熟市',
@@ -95,12 +101,16 @@ const configs = {
     serviceSupport: '苏州晶程智慧科技有限公司',
     code: changshuCode,
     layer: 'http://data.jingan-china.cn/v2/login/changshu_download_layer.png',
+    ios: "itms-services://?action=download-manifest&url=https://www.jingan-china.cn/download/cs/official_version/CSInterSafeZ.plist&ran=",
+    android: "http://58.215.178.100:12080/acloud_new/v2/mobileVersion/version/getLatest?type=1&ran=",
   },
   eye: {},
   xuexiao: {
     projectKey: 'v2_test',
     projectShortName: '校园智慧安全云',
     servicePhone: '400-928-5656',
+    ios: "itms-services://?action=download-manifest&url=https://www.jingan-china.cn/download/campus/official_version/CAMPUSInterSafe.plist&ran=",
+    android: "http://58.215.178.100:12082/acloud_new/v2/mobileVersion/version/getLatest?type=1&ran=",
   },
   nanxiao: {
     region: '徐州',
@@ -121,6 +131,8 @@ const configs = {
       'http://data.jingan-china.cn/v2/login/nanxiao/2.png',
       'http://data.jingan-china.cn/v2/login/nanxiao/3.png',
     ],
+    ios: "itms-services://?action=download-manifest&url=https://www.jingan-china.cn/download/fire/official_version/FireInterSafe.plist&ran=",
+    android: "http://58.215.178.100:12084/acloud_new/v2/mobileVersion/version/getLatest?type=1&ran=",
   },
   shanxi: {
     region: '山西',
@@ -128,12 +140,16 @@ const configs = {
     projectShortName: '山西晶安智慧云',
     code: shanxiCode,
     layer: 'http://data.jingan-china.cn/v2/login/shanxi_download_layer.png',
+    ios: "itms-services://?action=download-manifest&url=https://www.jingan-china.cn/download/sx/official_version/SXInterSafe.plist&ran=",
+    android: "http://58.215.178.100:12085/acloud_new/v2/mobileVersion/version/getLatest?type=1&ran=",
   },
   show: {
     region: '无锡',
     projectKey: 'yanshi_pro',
     projectShortName: '晶安智慧云',
     layer: 'http://data.jingan-china.cn/v2/login/show_download_layer.png',
+    ios: "itms-services://?action=download-manifest&url=https://www.jingan-china.cn/download/yanshi/official_version/yanshi.plist&ran=",
+    android: "http://58.215.178.100:12086/acloud_new/v2/mobileVersion/version/getLatest?type=1&ran=",
   },
 };
 
