6d5ad02b1b8ec4591efc0321b89501e3fe734bf9
PanChengYan <cypan@jingan-china.net>
2019-02-19 09:52:36 +0800

    feat: add pages

--- src/pages/BigPlatform/Smoke/sections/BusinessDrawer.js
+++ src/pages/BigPlatform/Smoke/sections/BusinessDrawer.js
@@ -1,52 +1,20 @@
 import React, { PureComponent, Fragment } from 'react';
+import { DatePicker } from 'antd';
+import moment from 'moment';
 
 import styles from './AlarmDrawer.less';
 import {
-  DrawerCard,
   DrawerContainer,
   DrawerSection,
-  GraphSwitch,
-  OvSelect,
-  SearchBar,
+  ChartBar,
+  ChartRing,
 } from '@/pages/BigPlatform/NewFireControl/components/Components';
-import {
-  DotItem,
-  BussinessChartBar,
-  BussinessChartLine,
-  AlarmChartBar,
-  AlarmChartLine,
-} from '../components/Components';
 
 const TYPE = 'business';
-const NO_DATA = '暂无信息';
-// const LABELS = ['未处理报警', '未处理故障'];
-// const COLORS = ['248,51,41', '255,180,0'];
-const OPTIONS = ['  全部业务 ', '未处理故障', '未处理报警'].map((d, i) => ({ value: i, desc: d }));
+const { RangePicker } = DatePicker;
 
 export default class BusinessDrawer extends PureComponent {
-  state = { graph: 0, otherGraph: 0, selected: 0, searchValue: '' };
-
-  handleSwitch = i => {
-    this.setState({ graph: i });
-  };
-
-  handleSwitchAlarm = i => {
-    this.setState({ otherGraph: i });
-  };
-
-  handleSelectChange = i => {
-    this.setState({ selected: i });
-  };
-
-  genProgressClick = v => {
-    return e => {
-      this.handleSelectChange(v);
-    };
-  };
-
-  handleSearch = v => {
-    this.setState({ searchValue: v });
-  };
+  state = { selected: 0, searchValue: '' };
 
   handleClose = () => {
     const { handleDrawerVisibleChange } = this.props;
@@ -55,170 +23,32 @@ export default class BusinessDrawer extends PureComponent {
   };
 
   render() {
-    const { visible, data: { list = [], graphList = [] } = {} } = this.props;
-    const { graph, selected, searchValue, otherGraph } = this.state;
-
-    const faultList = [
-      {
-        date: '2018-03',
-        finishGas: 0,
-        untreatedGas: 0,
-      },
-      {
-        date: '2018-04',
-        finishGas: 0,
-        untreatedGas: 0,
-      },
-      {
-        date: '2018-05',
-        finishGas: 0,
-        untreatedGas: 0,
-      },
-      {
-        date: '2018-06',
-        finishGas: 0,
-        untreatedGas: 0,
-      },
-      {
-        date: '2018-07',
-        finishGas: 0,
-        untreatedGas: 0,
-      },
-      {
-        date: '2018-08',
-        finishGas: 0,
-        untreatedGas: 0,
-      },
-      {
-        date: '2018-09',
-        finishGas: 0,
-        untreatedGas: 0,
-      },
-      {
-        date: '2018-10',
-        finishGas: 0,
-        untreatedGas: 0,
-      },
-      {
-        date: '2018-11',
-        finishGas: 0,
-        untreatedGas: 0,
-      },
-      {
-        date: '2018-12',
-        finishGas: 0,
-        untreatedGas: 0,
-      },
-      {
-        date: '2019-01',
-        finishGas: 0,
-        untreatedGas: 0,
-      },
-      {
-        date: '2019-02',
-        finishGas: 0,
-        untreatedGas: 0,
-      },
-    ];
-
-    const filteredList = list
-      .filter(({ company_name }) => company_name.includes(searchValue))
-      .filter(item => {
-        switch (selected) {
-          case 0:
-            return true;
-          case 1:
-            return item.common;
-          case 2:
-            return item.gasFire;
-          default:
-            return false;
-        }
-      });
-
-    const extra = <GraphSwitch handleSwitch={this.handleSwitch} />;
-    const extraOther = <GraphSwitch handleSwitch={this.handleSwitchAlarm} />;
-    const select = (
-      <OvSelect
-        cssType={1}
-        options={OPTIONS}
-        value={selected}
-        handleChange={this.handleSelectChange}
-      />
-    );
+    const { visible, data: { graphList = [] } = {} } = this.props;
 
     const left = (
       <Fragment>
-        <DrawerSection title="报警业务处理统计" titleInfo="最近12个月" extra={extra}>
-          {graph ? (
-            graphList.length > 0 ? (
-              <BussinessChartBar data={graphList} labelRotate={0} />
-            ) : (
-              <div style={{ height: '22em', textAlign: 'center', lineHeight: '22em' }}>
-                暂无数据
-              </div>
-            )
-          ) : graphList.length > 0 ? (
-            <BussinessChartLine data={graphList} labelRotate={0} />
-          ) : (
-            <div style={{ height: '22em', textAlign: 'center', lineHeight: '22em' }}>暂无数据</div>
-          )}
+        <RangePicker
+          format="YYYY-MM-DD HH:mm:ss"
+          placeholder={['开始时间', '结束时间']}
+          showTime={{
+            defaultValue: [moment('0:0:0', 'HH:mm:ss'), moment('23:59:59', 'HH:mm:ss')],
+          }}
+        />
+        <DrawerSection title="处理单位统计" style={{ marginBottom: 50 }}>
+          <ChartRing data={graphList} />
         </DrawerSection>
-        <DrawerSection title="故障业务处理统计" titleInfo="最近12个月" extra={extraOther}>
-          {otherGraph ? (
-            <AlarmChartBar data={faultList} labelRotate={0} />
-          ) : (
-            <AlarmChartLine data={faultList} labelRotate={0} />
-          )}
+        <DrawerSection title="火警单位排行" titleInfo="共n家">
+          <ChartBar data={graphList} labelRotate={-60} />
         </DrawerSection>
       </Fragment>
     );
 
-    const right = (
-      <SearchBar onSearch={this.handleSearch} extra={select}>
-        {filteredList.map(
-          ({
-            company_id,
-            company_name,
-            address,
-            principal_name,
-            principal_phone,
-            gasFire,
-            fault = 0,
-          }) => (
-            <div style={{ marginBottom: '10px' }}>
-              {gasFire > 0 && (
-                <DrawerCard
-                  key={company_id}
-                  name={company_name || NO_DATA}
-                  location={address || NO_DATA}
-                  person={principal_name || NO_DATA}
-                  phone={principal_phone || NO_DATA}
-                  style={{ cursor: 'auto' }}
-                  more={
-                    <p className={styles.more}>
-                      {gasFire > 0 && (
-                        <DotItem title="未处理报警" color={`rgb(248,51,41)`} quantity={gasFire} />
-                      )}
-                      {fault > 0 && (
-                        <DotItem title="未处理故障" color={`rgb(255,180,0)`} quantity={fault} />
-                      )}
-                    </p>
-                  }
-                />
-              )}
-            </div>
-          )
-        )}
-      </SearchBar>
-    );
-
     return (
       <DrawerContainer
-        title="待处理业务"
+        title="火警统计"
+        width={535}
         visible={visible}
         left={left}
-        right={right}
         placement="right"
         rowStyle={{ height: 'calc(100% - 70px)' }}
         onClose={this.handleClose}
