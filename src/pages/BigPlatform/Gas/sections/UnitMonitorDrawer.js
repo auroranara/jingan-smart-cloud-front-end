98b958922ccaf9b9c6147f3be32c6b88fa900b82
zangyunchong <yczang@jingan-china.cn>
2019-02-27 14:02:00 +0800

    fix: fix bug #3310

--- src/pages/BigPlatform/Gas/sections/UnitMonitorDrawer.js
+++ src/pages/BigPlatform/Gas/sections/UnitMonitorDrawer.js
@@ -67,11 +67,16 @@ export default class UnitMonitorDrawer extends PureComponent {
     return (<span className={styles.statusButton} style={{ borderColor: color, color }}>{desc}</span>)
   }
 
+  handleCLoseDrawer = () => {
+    const { onClose } = this.props
+    this.setState({ videoVisible: false })
+    onClose()
+  }
+
   render() {
     const {
       visible,
       title,
-      onClose,
       unitInfo,
       unitAbnormalTrend = [],
       handleChangeStatus,
@@ -192,7 +197,7 @@ export default class UnitMonitorDrawer extends PureComponent {
                   }}></div>
                   <div className={styles.contentContainer}>
                     <Ellipsis className={styles.line} lines={1} tooltip>{area ? (location ? `${area}：${location}` : area) : (location || '暂无位置数据')}</Ellipsis>
-                    <Ellipsis className={styles.line} lines={1} tooltip>LEL值：{realtime_data ? <span style={{ color: '#F83329' }}>{realtime_data}%</span> : '暂无数据'}</Ellipsis>
+                    <Ellipsis className={styles.line} lines={1} tooltip>LEL值：{realtime_data ? <span style={{ color: +status === 1 ? '#F83329' : 'inherit' }}>{realtime_data}%</span> : '暂无数据'}</Ellipsis>
                     <Ellipsis className={styles.line} lines={1} tooltip>参考值范围：{condition && limit_value ? `${condition}${limit_value}%` : '暂无数据'}</Ellipsis>
                     <div className={styles.lastLine}>
                       <div className={styles.camera} onClick={this.handleClickCamera} style={{
@@ -238,7 +243,7 @@ export default class UnitMonitorDrawer extends PureComponent {
         left={left}
         visible={visible}
         placement="right"
-        onClose={onClose}
+        onClose={this.handleCLoseDrawer}
       />
     )
   }
