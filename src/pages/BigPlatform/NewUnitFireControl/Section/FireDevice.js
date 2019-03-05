fdc3752d81b6a92ac8311ab8e92808476d92ae1b
PanChengYan <cypan@jingan-china.net>
2019-03-05 17:35:20 +0800

    feat(nanxiao): add pages

--- src/pages/BigPlatform/NewUnitFireControl/Section/FireDevice.js
+++ src/pages/BigPlatform/NewUnitFireControl/Section/FireDevice.js
@@ -1,93 +1,115 @@
 import React, { PureComponent } from 'react';
-import { Carousel } from 'antd';
+import { Tabs } from 'antd';
+import ReactEcharts from 'echarts-for-react';
 
 import Section from '../Section';
 import styles from './FireDevice.less';
 
-import normal from '../imgs/normal.png';
-import fine from '../imgs/fine.png';
-import error from '../imgs/error.png';
+function callback(key) {
+  console.log(key);
+}
+
+const TabPane = Tabs.TabPane;
 
 export default class FireDevice extends PureComponent {
-
-  getImageByStatus = (status) => {
-    switch (+status) {
-      case 1:
-        return error;
-      case 2:
-        return normal;
-      case 3:
-        return fine;
-      default:
-        return;
-    }
-  }
-
+  getOption = () => {
+    const option = {
+      tooltip: {
+        formatter: '{a} <br/>{c} {b}',
+      },
+      series: [
+        {
+          name: '速度',
+          type: 'gauge',
+          min: '',
+          max: '',
+          splitNumber: 5,
+          radius: '100%',
+          axisLine: {
+            // 坐标轴线
+            lineStyle: {
+              // 属性lineStyle控制线条样式
+              color: [[0.09, '#ff4400'], [0.82, '#1e90fe'], [1, '#ff4400']],
+              width: 1,
+            },
+          },
+          axisLabel: {
+            // 坐标轴小标记
+            textStyle: {
+              // 属性lineStyle控制线条样式
+              color: '#fff',
+            },
+          },
+          axisTick: {
+            // 坐标轴小标记
+            length: 8, // 属性length控制线长
+            lineStyle: {
+              // 属性lineStyle控制线条样式
+              color: 'auto',
+            },
+          },
+          splitLine: {
+            // 分隔线
+            length: 2, // 属性length控制线长
+            lineStyle: {
+              // 属性lineStyle（详见lineStyle）控制线条样式
+              width: 1,
+              color: '#fff',
+            },
+          },
+          pointer: {
+            // 分隔线
+            shadowColor: '#fff', //默认透明
+            shadowBlur: 2,
+          },
+          detail: {
+            borderColor: '#fff',
+            shadowColor: '#fff', //默认透明
+            shadowBlur: 2,
+            offsetCenter: [0, '50%'], // x, y，单位px
+            textStyle: {
+              // 其余属性默认使用全局文本样式，详见TEXTSTYLE
+              fontWeight: 'bolder',
+              color: '#fff',
+              fontSize: 16,
+            },
+          },
+          data: [{ value: 40 }],
+        },
+      ],
+    };
+    return option;
+  };
   render() {
-    const {
-      systemScore: { list = [] },
-      onClick,
-    } = this.props;
+    // const {
+    //   // onClick,
+    // } = this.props;
 
-    // 移除没有状态的成员
-    const arr = list.filter(({ status }) => status);
-    // 分为4个一组
-    const result = arr.reduce((total, item, index) => {
-      const i = Math.floor(index / 4);
-      if (index % 4 === 0) {
-        total[i] = [item];
-      }
-      else {
-        total[i].push(item);
-      }
-      return total;
-    }, [[]]);
     return (
-      <Section title="消防设施情况">
+      <Section title="水系统">
         <div className={styles.container}>
-          <Carousel autoplay autoplaySpeed={5000}>
-            {/* {result.map((cols, index) => {
-              const [{ sysName }] = cols;
-              return (
-                <div key={sysName} className={styles.wrapper}>
-                  {cols.map(({ sysId, sysName, status }) => (
-                    <div className={styles.item} key={sysName} onClick={() => { onClick({ sysId, sysName }) }}>
-                      <div className={styles.icon} style={{ backgroundImage: `url(${this.getImageByStatus(status)})` }} />
-                      <div className={styles.label}>{sysName}</div>
-                    </div>
-                  ))}
-                </div>
-              );
-            })} */}
-            <div className={styles.wrapper}>
-              {result[0].map(({ sysId, sysName, status }) => (
-                <div className={styles.item} key={sysName} onClick={() => { onClick({ sysId, sysName }) }}>
-                  <div className={styles.icon} style={{ backgroundImage: `url(${this.getImageByStatus(status)})` }} />
-                  <div className={styles.label}>{sysName}</div>
-                </div>
-              ))}
-            </div>
-            {result.length > 1 && (
-              <div className={styles.wrapper}>
-                {result[1].map(({ sysId, sysName, status }) => (
-                  <div className={styles.item} key={sysName} onClick={() => { onClick({ sysId, sysName }) }}>
-                    <div className={styles.icon} style={{ backgroundImage: `url(${this.getImageByStatus(status)})` }} />
-                    <div className={styles.label}>{sysName}</div>
-                  </div>
-                ))}
+          <Tabs defaultActiveKey="1" onChange={callback}>
+            <TabPane tab="消火栓系统" key="1">
+              <div className={styles.gauge}>
+                <ReactEcharts
+                  option={this.getOption()}
+                  style={{ width: '100px', height: '100px' }}
+                  className="echarts-for-echarts"
+                />
+                <ReactEcharts
+                  option={this.getOption()}
+                  style={{ width: '100px', height: '100px' }}
+                  className="echarts-for-echarts"
+                />
               </div>
-            )}
-            {result.length > 2 && (
-              <div className={styles.wrapper}>
-                {result[2].map(({ sysId, sysName, status }) => (
-                  <div className={styles.item} key={sysName} onClick={() => { onClick({ sysId, sysName }) }}>
-                    <div className={styles.icon} style={{ backgroundImage: `url(${this.getImageByStatus(status)})` }} />
-                    <div className={styles.label}>{sysName}</div>
-                  </div>
-                ))}
-              </div>
-            )}
-          </Carousel>
+            </TabPane>
+            <TabPane tab="自动喷淋系统" key="2">
+              2
+            </TabPane>
+            <TabPane tab="水池/水箱" key="3">
+              3
+            </TabPane>
+          </Tabs>
         </div>
       </Section>
     );
