7fb45cc9f497eaab9b28f5fd123fc796e4298f1e
luhua <neo.lu@foxmail.com>
2019-07-03 17:30:51 +0800

    fix(audio): fix default

--- src/utils/audio.js
+++ src/utils/audio.js
@@ -1,9 +1,9 @@
 const DEFAULT_SRC = 'http://data.jingan-china.cn/5a03005e74406.mp3';
 
 export default class BigScreenAudio {
-  constructor(src) {
+  constructor(src=DEFAULT_SRC) {
     const audio = this.audio = new Audio();
-    audio.src = src || DEFAULT_SRC;
+    audio.src = src;
   }
 
   play() {
