ab2fc0faf645bc7883bb0f730bababa4780154b7
wangyun <ywang@jingan-china.net>
2019-07-09 20:53:21 +0800

    fix: 增加版本信息

--- src/pages/User/Login/index.js
+++ src/pages/User/Login/index.js
@@ -545,9 +545,9 @@ export default class Login extends PureComponent {
             <div>
               <span>copyright©2019</span>
               <span>{unitName}</span>
+              <span style={{ marginLeft: '5px' }}>{global.VERSION}</span>
             </div>
             <div>
-              {/*
               <span>
                 运营支持：
                 {serviceSupport}
@@ -555,7 +555,7 @@ export default class Login extends PureComponent {
               <span>
                 服务电话：
                 {servicePhone}
-              </span> */}
+              </span>
             </div>
           </div>
         </div>
