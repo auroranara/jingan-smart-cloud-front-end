c11892beb360e599585541555358d5047cf47925
wangyun <ywang@jingan-china.net>
2019-07-11 09:20:37 +0800

    fix: 一些打包配置的优化

--- src/pages/User/Login/index.js
+++ src/pages/User/Login/index.js
@@ -548,14 +548,18 @@ export default class Login extends PureComponent {
               <span style={{ marginLeft: '5px' }}>{global.VERSION}</span>
             </div>
             <div>
-              <span>
-                运营支持：
-                {serviceSupport}
-              </span>
-              <span>
-                服务电话：
-                {servicePhone}
-              </span>
+              {serviceSupport && (
+                <span>
+                  运营支持：
+                  {serviceSupport}
+                </span>
+              )}
+              {servicePhone && (
+                <span>
+                  服务电话：
+                  {servicePhone}
+                </span>
+              )}
             </div>
           </div>
         </div>
