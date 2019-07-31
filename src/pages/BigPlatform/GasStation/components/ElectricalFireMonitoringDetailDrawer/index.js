94e3a8c14f448487f60f8bc44f28e07c46868ed2
sk <ksun@jingan-china.net>
2019-07-31 13:59:03 +0800

    fix: fix bug #6178

--- src/pages/BigPlatform/GasStation/components/ElectricalFireMonitoringDetailDrawer/index.js
+++ src/pages/BigPlatform/GasStation/components/ElectricalFireMonitoringDetailDrawer/index.js
@@ -91,7 +91,17 @@ export default class ElectricalFireMonitoringDetailDrawer extends PureComponent 
   }
 
   getGaugeOption = ({ name, value, unit, min, max, normalMin, normalMax, status }) => {
-    const color = status > 0 ? '#f83329' : '#fff';
+    let color, itemColor;
+    if (status > 0) {
+      color = '#f83329';
+      itemColor = '#ff1e00';
+    } else if (status < 0) {
+      color = '#ccc';
+      itemColor = '#ccc';
+    } else {
+      color = '#fff';
+      itemColor = '#1e90ff';
+    }
     return {
       series: [
         {
@@ -109,6 +119,9 @@ export default class ElectricalFireMonitoringDetailDrawer extends PureComponent 
               ].filter(v => v),
               width: 15,
             },
+          },
+          itemStyle: {
+            color: itemColor,
           },
           splitLine: {
             length: 15,
