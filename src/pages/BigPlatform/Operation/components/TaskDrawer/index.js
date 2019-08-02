6a0cb455a952c694004d7a34d730ae170d541f7a
sk <ksun@jingan-china.net>
2019-08-02 14:17:47 +0800

    fix: 修改运营大屏工单抽屉发生时间

--- src/pages/BigPlatform/Operation/components/TaskDrawer/index.js
+++ src/pages/BigPlatform/Operation/components/TaskDrawer/index.js
@@ -72,8 +72,8 @@ const FIELDNAMES = {
   area: ({ area, reportType }) => (typeDict2[reportType] === '消防主机' ? undefined : area), // 区域
   location: ({ installAddress, location, reportType }) =>
     typeDict2[reportType] === '消防主机' ? installAddress : location, // 位置
-  startTime: ({ createDate, reportType, firstTime }) =>
-    ({ 消防主机: firstTime, 独立烟感: firstTime, 报修: createDate }[typeDict2[reportType]]), // 报警/报修时间
+  startTime: ({ createDate, reportType, lastTime }) =>
+    ({ 消防主机: lastTime, 独立烟感: lastTime, 报修: createDate }[typeDict2[reportType]]), // 报警/报修时间
   endTime: 'endDate', // 结束时间
   status: ({ fireType }) => statusDict2[fireType], // 状态
   systemType: 'systemTypeValue', // 系统类型
