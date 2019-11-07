9198fbae1fd47174a52f84b1a077c184edc2f6ff
zhoukegang <kgzhou@jingan-china.net>
2019-11-07 16:40:41 +0800

    feat: esmap

--- src/pages/BigPlatform/Chemical/sections/Components.js
+++ src/pages/BigPlatform/Chemical/sections/Components.js
@@ -7,5 +7,17 @@ import Tips from './Tips';
 import CompanyInfo from './CompanyInfo';
 import Notice from './Notice';
 import HiddenDanger from './HiddenDanger';
+import Map from './Map';
 
-export { Risk, KeyPoints, HighDanger, Emergency, Remind, Tips, CompanyInfo, Notice, HiddenDanger };
+export {
+  Risk,
+  KeyPoints,
+  HighDanger,
+  Emergency,
+  Remind,
+  Tips,
+  CompanyInfo,
+  Notice,
+  HiddenDanger,
+  Map,
+};
