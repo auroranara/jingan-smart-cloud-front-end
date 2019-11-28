88b5b98157530d8746dd6a7db3e311e8b4064ee6
sk <18921261761@163.com>
2019-11-28 18:01:49 +0800

    feat: temp

--- src/pages/AccidentManagement/QuickReport/TableList.js
+++ src/pages/AccidentManagement/QuickReport/TableList.js
@@ -1,28 +1,84 @@
 import React, { PureComponent } from 'react';
-// import { connect } from 'dva';
+import { connect } from 'dva';
 import router from 'umi/router';
 import { Button, Card, Table } from 'antd';
-
 import ToolBar from '@/components/ToolBar';
 import PageHeaderLayout from '@/layouts/PageHeaderLayout';
+import { getPageSize, setPageSize } from '@/utils/utils';
 import styles1 from '@/pages/SafetyKnowledgeBase/MSDS/MList.less';
 import { BREADCRUMBLIST, LIST, PAGE_SIZE, ROUTER, SEARCH_FIELDS as FIELDS, TABLE_COLUMNS as COLUMNS } from './utils';
 
+const GET_LIST = 'accidentReport/getList';
+
+@connect(({
+  user,
+  accidentReport,
+  loading,
+}) => ({
+  user,
+  accidentReport,
+  loading: loading.effetcs[GET_LIST],
+}), (dispatch) => ({
+  getList(payload, callback) {
+    dispatch({
+      type: GET_LIST,
+      payload,
+      callback,
+    });
+  },
+}))
 export default class TableList extends PureComponent {
+  componentDidMount() {
+    this.getList();
+  }
+
+  getList = (payload) => {
+    const {
+      accidentReport: {
+        list: {
+          pagination: {
+            pageSize: prevPageSize=getPageSize(),
+          }={},
+        }={},
+      },
+      getList,
+    } = this.props;
+    const { current=1, pageSize=getPageSize() } = payload || {};
+    const values = this.form && this.form.getFieldsValue();
+    getList({
+      pageNum: prevPageSize !== pageSize ? 1 : current,
+      pageSize,
+      ...values,
+      ...payload,
+    });
+    setPageSize(pageSize);
+  }
+
+  reload = () => {
+    const {
+      accidentReport: {
+        list: {
+          pagination: {
+            pageNum=1,
+          }={},
+        }={},
+      },
+    } = this.props;
+    this.getList({
+      current: pageNum,
+    });
+  }
+
   handleSearch = values => {
-    return;
+    this.getList(values);
   };
 
-  handleReset = () => {
-    return;
+  handleReset = (values) => {
+    this.getList(values);
   };
 
   handleAdd = () => {
     router.push(`${ROUTER}/add`);
-  };
-
-  onTableChange = (pagination, filters, sorter) => {
-    return;
   };
 
   render() {
