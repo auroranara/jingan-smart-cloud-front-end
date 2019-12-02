005e618378d686fe2a74ecbad4ab72b85c233ff0
luhua <neo.lu@foxmail.com>
2019-12-02 20:49:58 +0800

    fix(router): add exception

--- config/router.config.js
+++ config/router.config.js
@@ -316,6 +316,23 @@ module.exports = env => {
             },
           ],
         },
+        {
+          name: 'exception',
+          icon: 'warning',
+          path: '/exception',
+          hideInMenu: true,
+          routes: [
+            { path: '/exception/403', name: 'not-permission', component: './Exception/403' },
+            { path: '/exception/404', name: 'not-find', component: './Exception/404' },
+            { path: '/exception/500', name: 'server-error', component: './Exception/500' },
+            {
+              path: '/exception/trigger',
+              name: 'trigger',
+              hideInMenu: true,
+              component: './Exception/triggerException',
+            },
+          ],
+        },
 
         {
           path: '/system-management', // 系统管理
