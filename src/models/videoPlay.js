97bd90ae04ea553ffcf89f4a76a4a39a517c4afd
wangyun <ywang@jingan-china.net>
2020-01-20 15:38:56 +0800

    fix: 增加视频key_id的判断

--- src/models/videoPlay.js
+++ src/models/videoPlay.js
@@ -12,17 +12,15 @@ export default {
     // 'http://anbao.wxjy.com.cn/hls/xsfx_jiefanglu.m3u8'
     // http://218.90.184.178:23389/hls/dangkou/test.m3u8
     *fetchStartToPlay({ payload, success, error }, { call, put, select }) {
-      // let videoId = yield select(state => state.videoPlay.videoId);
-      // console.log('开始的videoId', videoId);
-      // console.log('payload.key_id', payload.key_id);
-      // 只有当一开始没有视频或者请求的key与返回的key相同时候 才会播放
-      // if (videoId && payload.key_id !== videoId) return;
+      console.log('11111');
+      console.log('payload', payload);
+
       yield put({
         type: 'saveVideoId',
         payload: { videoId: payload.key_id },
       });
+      if (!payload.key_id) return;
       const response = yield call(getStartToPlay, payload);
-      // fxck 沙雕接口 该设备ID不存在也回复200
       if (response && response.code === 200 && response.data.url !== '该设备ID不存在') {
         let videoId = yield select(state => state.videoPlay.videoId);
         if (videoId && response.data.key_id !== videoId) return;
