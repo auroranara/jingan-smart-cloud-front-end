acea08db096d7577ad9b08d99941be000a4be4f6
PanChengYan <cypan@jingan-china.net>
2020-03-19 16:51:44 +0800

    fix: fix bug #8459, #8455

--- config/config.js
+++ config/config.js
@@ -47,7 +47,7 @@ const hosts = {
 export default {
   proxy: {
     '/acloud_new': {
-      target: `http://${hosts.gj}`,
+      target: `http://${hosts.test}`,
       changeOrigin: true,
       pathRewrite: { '^/acloud_new': '/acloud_new' },
     },
@@ -77,12 +77,12 @@ export default {
         dynamicImport: true,
         ...(os.platform() === 'darwin'
           ? {
-            dll: {
-              include: ['dva', 'dva/router', 'dva/saga', 'dva/fetch'],
-              exclude: ['@babel/runtime'],
-            },
-            hardSource: false,
-          }
+              dll: {
+                include: ['dva', 'dva/router', 'dva/saga', 'dva/fetch'],
+                exclude: ['@babel/runtime'],
+              },
+              hardSource: false,
+            }
           : {}),
       },
     ],
