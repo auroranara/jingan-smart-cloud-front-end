a31200d970a7bb0ea8e78b64524b7f98a96cff37
luhua <neo.lu@foxmail.com>
2020-03-24 17:26:41 +0800

    fix(position): add  trackSn

--- src/pages/PersonnelPositionNew/utils.js
+++ src/pages/PersonnelPositionNew/utils.js
@@ -3,7 +3,7 @@ const BUILD_ID = '201060';
 // const SECRET = '8691844ea20d4ceeaa3d000ebc3a5ba1';
 const APP_ID = 'client';
 
-export function getSrc(type, ip, buildId, secret, appId) {
-  return `http://${ip}/js/tunnel.html?to=${type}&buildId=${buildId || BUILD_ID}&appid=${appId || APP_ID}&secret=${secret}&wh=false`;
+export function getSrc(type, ip, buildId, secret, appId, trackSn) {
+  return `http://${ip}/js/tunnel.html?to=${type}&buildId=${buildId || BUILD_ID}&appid=${appId || APP_ID}&secret=${secret}&wh=false${trackSn === undefined ? '' : `&trackSn=${trackSn}`}`;
   // return `http://chem2.joysuch.com/js/tunnel.html?to=${type}&buildId=200647&wh=false&appid=yanshi&secret=4011a04a6615406a9bbe84fcf30533de`;
 }
