528309427209d8d7a32bb7e2f257cdba994e1073
PanChengYan <cypan@jingan-china.net>
2020-04-09 11:03:13 +0800

    fix: bug

--- src/pages/RealNameCertification/VisitorRegistration/utils.js
+++ src/pages/RealNameCertification/VisitorRegistration/utils.js
@@ -237,7 +237,7 @@ export const RECORD_FIELDS = [
       <RangePicker
         showTime={{
           hideDisabledOptions: true,
-          defaultValue: [moment('00:00:00', 'HH:mm:ss'), moment('11:59:59', 'HH:mm:ss')],
+          defaultValue: [moment('00:00:00', 'HH:mm:ss'), moment('00:00:00', 'HH:mm:ss')],
         }}
         format="YYYY-MM-DD HH:mm:ss"
       />
