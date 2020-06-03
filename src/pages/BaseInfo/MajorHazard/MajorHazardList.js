554abb37c999a946f077fbbaf3538414e42b075e
luhua <neo.lu@foxmail.com>
2020-06-03 09:47:53 +0800

    fix(table): target=_blank

--- src/pages/BaseInfo/MajorHazard/MajorHazardList.js
+++ src/pages/BaseInfo/MajorHazard/MajorHazardList.js
@@ -299,13 +299,13 @@ export default class MajorHazardList extends PureComponent {
         render: (val, row) => (
           <Fragment>
             {viewCode ? (
-              <Link to={`/major-hazard-info/major-hazard/detail/${row.id}`}>查看</Link>
+              <Link to={`/major-hazard-info/major-hazard/detail/${row.id}`} target="_blank">查看</Link>
             ) : (
               <span style={{ cursor: 'not-allowed', color: 'rgba(0, 0, 0, 0.25)' }}>查看</span>
             )}
             <Divider type="vertical" />
             {editCode ? (
-              <Link to={`/major-hazard-info/major-hazard/edit/${row.id}`}>编辑</Link>
+              <Link to={`/major-hazard-info/major-hazard/edit/${row.id}`} target="_blank">编辑</Link>
             ) : (
               <span style={{ cursor: 'not-allowed', color: 'rgba(0, 0, 0, 0.25)' }}>编辑</span>
             )}
