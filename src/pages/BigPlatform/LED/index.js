ea836de29860436ebc8415152e15649cb5b7adf7
luhua <neo.lu@foxmail.com>
2020-06-23 12:19:26 +0800

    fix(led): add led table

--- src/pages/BigPlatform/LED/index.js
+++ src/pages/BigPlatform/LED/index.js
@@ -15,6 +15,8 @@ const SocketOptions = {
 };
 
 const DELAY = 60000 * 10;
+const SWITCH_DELAY = 20000;
+const YES_OR_NO = ['否', '是'];
 
 @connect(({ loading, bigPlatform, twoInformManagement }) => ({
   twoInformManagement,
@@ -30,10 +32,12 @@ export default class Led extends PureComponent {
       createTime: '',
       height: 0,
       scroll: false,
+      displayIndex: 0,
     };
   }
 
   pollTimer = null;
+  swithTimer = null;
 
   componentDidMount() {
     this.fetchCompanyMsg();
@@ -44,16 +48,22 @@ export default class Led extends PureComponent {
     this.initWebSocket();
     this.fetchSafetyPromise();
     this.pollTimer = setInterval(this.polling, DELAY);
+    this.swithTimer = setInterval(this.switchDisplay, SWITCH_DELAY);
   }
 
   componentWillUnmount() {
     clearInterval(this.pollTimer);
     clearInterval(this.myTimer);
+    clearInterval(this.swithTimer);
   }
 
   polling = () => {
     this.fetchSafetyPromise();
   };
+
+  switchDisplay = () => {
+    this.setState(({ displayIndex }) => ({ displayIndex: (displayIndex + 1) % 2 }));
+  }
 
   // 获取统计初始化数据
   fetchLedData = () => {
@@ -164,11 +174,13 @@ export default class Led extends PureComponent {
         companyMessage: {
           companyMessage: { companyName },
         },
-        ledPromise: { contentForLED, dutyPerson, createTime },
+        ledPromise: { allContent, dutyPerson, createTime },
       },
     } = this.props;
-    const { personData, currentTime, height, scroll } = this.state;
+    const { personData, currentTime, displayIndex } = this.state;
     const personList = Array.isArray(personData) ? personData.slice(1) : [];
+    const [total, run, stop, checking, levelTwo, levelOne, specialWork, high, limitedSpace, ground, short, breaker, wall, electricity, other, pilot, driving, safe] =
+      allContent ? allContent.split(',').map(s => s.trim()) : Array(18).fill(0);
 
     return (
       <div className={styles.content}>
@@ -176,40 +188,81 @@ export default class Led extends PureComponent {
           <div className={styles.time}>{currentTime}</div>
           <div className={styles.label}>{companyName}</div>
         </div>
-        <div className={styles.seaction}>
-          <div className={styles.left}>
-            <div className={styles.title}>{personData[0]}</div>
-            <div className={styles.itemArea}>
-              {personList.length ? (
-                <Scroll autoScroll autoHide>
-                  {personList.length > 0
-                    ? personList.map((item, index) => (
-                        <div className={styles.item} key={index}>
-                          {item}
-                        </div>
-                      ))
-                    : []}
-                </Scroll>
-              ) : null}
-            </div>
-          </div>
-          <div className={styles.right}>
-            <div className={styles.title}>安全承诺公告</div>
-            {/* <div className={styles.item}>
-              今天我公司已经进行了安全风险研判，各项安全风险防控措施落实到位，我承诺所有生产装置处于安全运行状态，罐区、仓库等重大危险源安全风险得到有效控制。
-            </div>
-            <div className={styles.itemExtra}>负责人：周新</div> */}
-            {contentForLED && (
-              <div className={styles.promise} id="led-promise">
-                <div style={{ height }} className={styles.promiseOuter}>
-                  <div className={styles[`promiseInner${scroll ? '1' : ''}`]} ref={this.onPromiseRef}>
-                    <div>{contentForLED}</div>
-                    <div className={styles.itemExtra}>{dutyPerson}</div>
-                    <div className={styles.itemExtra}>{moment(createTime).format('YYYY-MM-DD')}</div>
-                  </div>
+        <div className={styles.sectionsContainer}>
+          <div className={styles.sections} style={{ left: `calc(-100% * ${displayIndex})` }}>
+            <div className={styles.section}>
+              <div className={styles.left}>
+                <div className={styles.title}>{personData[0]}</div>
+                <div className={styles.itemArea}>
+                  {personList.length ? (
+                    <Scroll autoScroll autoHide>
+                      {personList.length > 0
+                        ? personList.map((item, index) => (
+                            <div className={styles.item} key={index}>
+                              {item}
+                            </div>
+                          ))
+                        : []}
+                    </Scroll>
+                  ) : null}
                 </div>
               </div>
-            )}
+              <div className={styles.right}>
+                <div className={styles.title}>安全承诺公告</div>
+                <div className={styles.item}>
+                  今天我公司已经进行了安全风险研判，各项安全风险防控措施落实到位，我承诺所有生产装置处于安全运行状态，罐区、仓库等重大危险源安全风险得到有效控制。
+                </div>
+                <div className={styles.itemExtra}>{dutyPerson}</div>
+                <div className={styles.itemExtra}>{moment().format('YYYY-MM-DD')}</div>
+              </div>
+            </div>
+            <div className={styles.section1}>
+              <table className={styles.table}>
+                <tr>
+                  <th colSpan="4">
+                    企业状态
+                  </th>
+                </tr>
+                <tr>
+                  <td>生产装置{total}套</td>
+                  <td>其中运行{run}套</td>
+                  <td>停产{stop}套</td>
+                  <td>检修{checking}套</td>
+                </tr>
+                <tr>
+                  <td rowSpan="4">特殊作业</td>
+                  <td>二级动火{levelTwo}处</td>
+                  <td>一级动火{levelOne}处</td>
+                  <td>特殊动火{specialWork}处</td>
+                </tr>
+                <tr>
+                  <td>高处作业{high}处</td>
+                  <td>受限空间作业{limitedSpace}处</td>
+                  <td>动土作业{ground}处</td>
+                </tr>
+                <tr>
+                  <td>短路作业{short}处</td>
+                  <td>断路作业{breaker}处</td>
+                  <td>盲板抽堵{wall}处</td>
+                </tr>
+                <tr>
+                  <td>临时用电{electricity}处</td>
+                  <td colSpan="2">其他作业{other}处</td>
+                </tr>
+                <tr>
+                  <td colSpan="4">是否处于试生产状态：{YES_OR_NO[pilot]}</td>
+                </tr>
+                <tr>
+                  <td colSpan="4">是否处于开车状态：{YES_OR_NO[driving]}</td>
+                </tr>
+                <tr>
+                  <td colSpan="4">是否处于停车状态：{YES_OR_NO[+!+driving]}</td>
+                </tr>
+                <tr>
+                  <td colSpan="4">罐区、仓库等重大危险源是否处于安全状态：{YES_OR_NO[safe]}</td>
+                </tr>
+              </table>
+            </div>
           </div>
         </div>
       </div>
