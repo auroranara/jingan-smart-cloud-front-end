dbc931f63ecad883ccb5bb85eaea0b2533bf115e
luhua <neo.lu@foxmail.com>
2020-07-27 11:27:54 +0800

    fix(response): fix led response

--- config/config.js
+++ config/config.js
@@ -49,7 +49,7 @@ const hosts = {
 export default {
   proxy: {
     '/acloud_new': {
-      target: `http://${hosts.up}`,
+      target: `http://${hosts.test}`,
       changeOrigin: true,
       pathRewrite: { '^/acloud_new': '/acloud_new' },
     },
