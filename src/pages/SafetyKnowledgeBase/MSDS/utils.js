35f39d61c14833cc1d0a1a45c25000ec719a46ca
luhua <neo.lu@foxmail.com>
2020-08-14 09:34:23 +0800

    fix(radio): fix radio group

--- src/pages/SafetyKnowledgeBase/MSDS/utils.js
+++ src/pages/SafetyKnowledgeBase/MSDS/utils.js
@@ -177,12 +177,12 @@ function genFormItem(field, getFieldDecorator) {
           />
         );
         break;
+      // case 'radio':
+      //   component = (
+      //     <RadioGroup disabled={disabled} options={getOptions(options, ['value', 'label'])} />
+      //   );
+      //   break;
       case 'radio':
-        component = (
-          <RadioGroup disabled={disabled} options={getOptions(options, ['value', 'label'])} />
-        );
-        break;
-      case 'radios':
         component = (
           <RadioGroup disabled={disabled}>
             {getOptions(options, ['value', 'label']).map(({ value, label }) => <Radio key={value} value={value}>{label}</Radio>)}
